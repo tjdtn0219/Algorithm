3e5d7c8fcb3e41f7f4ab22dbfb91691085f78ae6
tjdtn0219 <76704436+tjdtn0219@users.noreply.github.com>
2023-01-17 16:37:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1464 ms, Memory: 130604 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,33 +1,27 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
- 
-public class Main {
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> arr = new ArrayList<>();
- 
-        for (int i = 0; i < N; i++) {
-            arr.add(Integer.parseInt(br.readLine()));
-        }
-        
-        Collections.sort(arr);
- 
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(arr.get(i) + "\n");
+
+        int N = Integer.parseInt(bf.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            list.add(Integer.parseInt(bf.readLine()));
         }
- 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+
+        Collections.sort(list);
+
+        for(int num : list){
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
     }
 }
