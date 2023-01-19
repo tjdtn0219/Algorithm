d5e5a246514452ebd50fd891837200b5ce9d31d5
tjdtn0219 <76704436+tjdtn0219@users.noreply.github.com>
2023-01-19 18:51:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1560 ms, Memory: 66140 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        List<Point> pList = new ArrayList<>();
+
+        for(int i=0; i<N; i++){
+            String[] str = bf.readLine().strip().split(" ");
+            Point point = new Point(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
+            pList.add(point);
+        }
+
+        Collections.sort(pList, new PointComparator());
+
+        for(Point p : pList){
+            System.out.println(p.x + " " + p.y);
+        }
+    }
+}
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class PointComparator implements Comparator<Point>{
+
+    @Override
+    public int compare(Point p1, Point p2) {
+        if(p1.x == p2.x) return p1.y - p2.y;
+        return p1.x - p2.x;
+    }
+}
