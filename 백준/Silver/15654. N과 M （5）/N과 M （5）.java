49855555ef079bac4606842ae4f345cf3c4a6fa0
tjdtn0219 <76704436+tjdtn0219@users.noreply.github.com>
2023-04-25 19:29:09 +0900

    [Silver III] Title: N과 M (5), Time: 1112 ms, Memory: 68612 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static int[] arr;
+    public static int[] temp;
+    public static boolean[] vis;
+    public static int N, M;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] strings = bf.readLine().split(" ");
+
+        N = Integer.parseInt(strings[0]);
+        M = Integer.parseInt(strings[1]);
+
+        arr = new int[N];
+        vis = new boolean[N];
+        temp = new int[M];
+
+        strings = bf.readLine().split(" ");
+        for(int i=0; i<strings.length; i++) {
+            arr[i] = Integer.parseInt(strings[i]);
+        }
+
+        Arrays.sort(arr);
+
+        recur(0);
+
+    }
+
+    public static void recur(int k) {
+
+        if(k==M) {
+            StringBuilder sb = new StringBuilder();
+            for(int num : temp) {
+                sb.append(num + " ");
+            }
+            System.out.println(sb);
+            return;
+        }
+
+
+        for(int i=0; i<arr.length; i++) {
+            if(!vis[i]) {
+                temp[k] = arr[i];
+                vis[i] = true;
+//                System.out.println("k : " + k);
+//                for (int j = 0; j < k + 1; j++) {
+//                    System.out.print(temp[j] + " ");
+//                }
+//                System.out.println("\n=================");
+                recur(k + 1);
+                vis[i] = false;
+            }
+        }
+    }
+
+}
