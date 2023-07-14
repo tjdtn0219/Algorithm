0802fffd8bd0301a08d5409ac5f494aee0235614
tjdtn0219 <76704436+tjdtn0219@users.noreply.github.com>
2023-07-14 23:48:50 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 772 ms, Memory: 171952 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,61 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-
-    public static List<Integer> vList[];
+    public static List<Integer>[] vList;
+    public static Queue<Integer> q = new LinkedList<>();
     public static boolean[] vis;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] inputs = bf.readLine().split(" ");
-
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
+        String[] strings = bf.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
 
         vis = new boolean[n+1];
         vList = new ArrayList[n+1];
-        for(int i=1; i<=n; i++) {
-            vList[i] = new ArrayList<Integer>();
+        for(int i=1; i<n+1; i++) {
+            vList[i] = new ArrayList<>();
         }
 
         for(int i=0; i<m; i++) {
-            inputs = bf.readLine().split(" ");
-
-            int s = Integer.parseInt(inputs[0]);
-            int e = Integer.parseInt(inputs[1]);
-
-            vList[s].add(e);
-            vList[e].add(s);
+            strings = bf.readLine().split(" ");
+            int v1 = Integer.parseInt(strings[0]);
+            int v2 = Integer.parseInt(strings[1]);
+            vList[v1].add(v2);
+            vList[v2].add(v1);
         }
 
-        int cnt = 0;
+        int ans = 0;
+
         for(int i=1; i<=n; i++) {
-            if(!vis[i]) {
-                cnt++;
-                bfs(i);
+            if (!vis[i]) {
+                q.add(i);
+                vis[i] = true;
+                bfs();
+                ans++;
             }
         }
-        System.out.println(cnt);
 
+        System.out.println(ans);
     }
 
-    public static void bfs(int num) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(num);
+    public static void bfs() {
 
-        while(!queue.isEmpty()) {
-            int popped = queue.poll();
-            for(int adj : vList[popped]) {
+        while(!q.isEmpty()) {
+            int popped = q.poll();
+            for(Integer adj : vList[popped]) {
                 if(vis[adj]) continue;
-                queue.add(adj);
+                q.add(adj);
                 vis[adj] = true;
             }
         }
     }
+
 }
