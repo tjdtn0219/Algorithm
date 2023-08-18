c7cbccb8ebd15e17f6519b8c6348a090e5572757
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-08-18 17:20:26 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 964 ms, Memory: 68164 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,39 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] inputs = bf.readLine().split(" ");
+        String[] strings = br.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int m = Integer.parseInt(strings[1]);
 
-        int N = Integer.parseInt(inputs[0]);
-        int M = Integer.parseInt(inputs[1]);
+        strings = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(strings[i]);
 
-        inputs = bf.readLine().split(" ");
-        int[] arr = new int[N];
-        int[] d = new int[N];
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(inputs[i]);
+        int[] sum = new int[n+1];
+        sum[1] = arr[1];
+        for(int i=1; i<n+1; i++) sum[i] = sum[i-1] + arr[i-1];
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<m; i++) {
+            strings = br.readLine().split(" ");
+            int st = Integer.parseInt(strings[0]);
+            int en = Integer.parseInt(strings[1]);
+            int ans = sum[en] - sum[st-1];
+            sb.append(ans + "\n");
         }
 
-        d[0] = arr[0];
-        for(int i=1; i<N; i++) {
-            d[i] = d[i - 1] + arr[i];
-        }
-
-        for(int i=0; i<M; i++) {
-            inputs = bf.readLine().split(" ");
-            int a = Integer.parseInt(inputs[0]);
-            int b = Integer.parseInt(inputs[1]);
-
-            if(a==1) System.out.println(d[b-1]);
-            else System.out.println(d[b-1]-d[a-2]);
-        }
+        System.out.println(sb);
 
     }
 }
