638b0f8db4be1118cc2e51dfc0a9812eb1e738c5
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-08-21 17:29:10 +0900

    [Silver I] Title: 1로 만들기 2, Time: 164 ms, Memory: 22428 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,40 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(bf.readLine());
-
-        int[] d = new int[n+1];
-        int[] prev = new int[n+1];
+        int[] d = new int[n + 1];
+        int[] prev = new int[n + 1];
 
         d[1] = 0;
-
-        for(int i=2; i<=n; i++) {
-            d[i] = d[i-1] + 1;
-            prev[i] = i-1;
-            if(i%2==0 && d[i] > d[i/2] + 1) {
-                d[i] = d[i/2] + 1;
-                prev[i] = i/2;
+        for (int i = 2; i <= n; i++) {
+            d[i] = d[i - 1] + 1;
+            prev[i] = i - 1;
+            if (i % 2 == 0) {
+                if (d[i] > d[i / 2] + 1) {
+                    d[i] = d[i / 2] + 1;
+                    prev[i] = i / 2;
+                }
             }
-            if(i%3==0 && d[i] > d[i/3] + 1) {
-                d[i] = d[i/3] + 1;
-                prev[i] = i/3;
+            if (i % 3 == 0) {
+                if (d[i] > d[i / 3] + 1) {
+                    d[i] = d[i / 3] + 1;
+                    prev[i] = i / 3;
+                }
             }
         }
-
         System.out.println(d[n]);
-        while(prev[n] > 0) {
+        while (prev[n] > 0) {
             System.out.print(n + " ");
             n = prev[n];
         }
         System.out.print(1);
-
     }
 }
