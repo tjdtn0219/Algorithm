12abd740ddb6f7accd86207c13d80e5ee702c0c9
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-10-08 17:16:25 +0900

    [Gold II] Title: 가운데를 말해요, Time: 544 ms, Memory: 38808 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,36 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-class Main
-{
-	
-	public static void main(String args[]) throws Exception {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
 
-		PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1,o2)->o2-o1);
-		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			
-			if(maxHeap.size() <= minHeap.size()) maxHeap.add(num);
-			else minHeap.add(num);
-			
-			if(minHeap.size()>0 && maxHeap.peek() > minHeap.peek()) {
-				minHeap.add(maxHeap.poll());
-				maxHeap.add(minHeap.poll());
-			}
-			
-			sb.append(maxHeap.peek() + "\n");
-		}
-		
-		System.out.print(sb.toString());
-		
-	}
-   
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(bf.readLine());
+
+        PriorityQueue<Integer> maxHeap =  new PriorityQueue<>(Collections.reverseOrder());    //중간값 왼쪽
+        PriorityQueue<Integer> minHeap =  new PriorityQueue<>();    //중간값 오른쪽
+
+        for(int i=0; i<n; i++) {
+            int num = Integer.parseInt(bf.readLine());
+
+            if(maxHeap.size() <= minHeap.size()) maxHeap.add(num);
+            else minHeap.add(num);
+
+            if(!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(minHeap.poll());
+            }
+
+            sb.append(maxHeap.peek() + "\n");
+        }
+        System.out.print(sb);
+    }
 }
-
