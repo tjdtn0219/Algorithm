abb8e8cb8fb3a3554032ed668f9750494a137c40
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-10-14 17:08:37 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1496 ms, Memory: 140996 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,17 +1,15 @@
+import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(bf.readLine());
-        String[] strings = bf.readLine().split(" ");
+        int n = Integer.parseInt(br.readLine());
+        String[] strings = br.readLine().split(" ");
 
         int[] a1 = new int[n];
         for(int i=0; i<n; i++) {
@@ -19,16 +17,32 @@ public class Main {
         }
         Arrays.sort(a1);
 
-        n = Integer.parseInt(bf.readLine());
-        strings = bf.readLine().split(" ");
+        n = Integer.parseInt(br.readLine());
+        strings = br.readLine().split(" ");
         int[] a2 = new int[n];
         for(int i=0; i<n; i++) {
             a2[i] = Integer.parseInt(strings[i]);
         }
 
         for(int tg : a2) {
-            sb.append(upper_idx(tg, a1) - lower_idx(tg, a1) + " ");
+//            sb.append(upper_idx(tg, a1) - lower_idx(tg, a1) + " ");
+            int li = lower_idx(tg, a1);
+            int ri = upper_idx(tg, a1);
+            if(li >= a1.length) sb.append(0 + " ");
+            else if(a1[li]!=tg) sb.append(0 + " ");
+            else sb.append(ri-li).append(" ");
         }
+//        n = Integer.parseInt(br.readLine());
+//        strings = br.readLine().split(" ");
+//        for(int i=0; i<n; i++) {
+//            int num = Integer.parseInt(strings[i]);
+//            int li = lower_idx(num, a1);
+//            int ri = upper_idx(num, a1);
+//            if(a1[li]!=num) sb.append(0 + " ");
+//            else sb.append(ri-li).append(" ");
+////            System.out.println("num : " + num + " li : " + li + " value : " + arr[li]);
+////            System.out.println("num : " + num + " ri : " + ri + " value : " + arr[li]);
+//        }
         System.out.println(sb.toString());
 
     }
