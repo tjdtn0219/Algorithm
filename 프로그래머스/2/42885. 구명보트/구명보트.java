d6df84d0a9de934a897b0526fa84320117fcda24
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-11-02 18:08:45 +0900

    [level 2] Title: 구명보트, Time: 28.86 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -10,11 +10,10 @@ class Solution {
         }
         Collections.sort(list);
         
-        // for(int w : arr) System.out.print(w + " ");
-        // System.out.println();
-        
         int left = 0;
         int right = list.size() - 1;
+        
+        //10 20 30 70 95 /// LMT = 100
         
         int sum = 0;
         while(left <= right) {
