fb8b153ab7713b8063d6c08c6d96c5bbb282b268
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2023-11-05 00:45:45 +0900

    [Gold II] Title: 가운데를 말해요, Time: 504 ms, Memory: 38188 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -2,29 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> left = new PriorityQueue<>((o1,o2)->o2-o1);  //맥스힙
+        PriorityQueue<Integer> right = new PriorityQueue<>();  //미니힙
+
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(bf.readLine());
-
-        PriorityQueue<Integer> maxHeap =  new PriorityQueue<>(Collections.reverseOrder());    //중간값 왼쪽
-        PriorityQueue<Integer> minHeap =  new PriorityQueue<>();    //중간값 오른쪽
-
-        for(int i=0; i<n; i++) {
-            int num = Integer.parseInt(bf.readLine());
-
-            if(maxHeap.size() <= minHeap.size()) maxHeap.add(num);
-            else minHeap.add(num);
-
-            if(!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-                minHeap.add(maxHeap.poll());
-                maxHeap.add(minHeap.poll());
+        //left.peek()가 무조건 중앙값임
+        left.add(Integer.parseInt(br.readLine()));
+        sb.append(left.peek() + "\n");
+        for(int i=1; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int centerValue = left.peek();
+            if(num <= centerValue) {
+                left.add(num);
+            } else {
+                right.add(num);
             }
 
-            sb.append(maxHeap.peek() + "\n");
+            if(left.size() > right.size()+1) {
+                right.add(left.poll());
+            }
+
+            if(left.size() < right.size()) {
+                left.add(right.poll());
+            }
+            sb.append(left.peek() + "\n");
         }
         System.out.print(sb);
+
     }
 }
