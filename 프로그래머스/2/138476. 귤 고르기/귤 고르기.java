8974136cedbbf384fb7382bbccec7354bdf98046
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-02-18 00:41:38 +0900

    [level 2] Title: 귤 고르기, Time: 55.98 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,52 +1,48 @@
 import java.util.*;
 
 class Solution {
+    
+    HashMap<Integer, Integer> cntMap;
+    PriorityQueue<Tangerine> pq;
+    
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for(int num : tangerine) {
-            int cnt = hmap.getOrDefault(num, 0);
-            hmap.put(num, cnt+1);
-        }
+        init(tangerine);
         
-        int total = tangerine.length;
-        
-        PriorityQueue<Pair> pq = new PriorityQueue<>((o1,o2)->o1.cnt-o2.cnt);
-        for(int key : hmap.keySet()) {
-            pq.add(new Pair(key, hmap.get(key)));
-        }
-        
-        // System.out.println(total);
-        
-        // while(!pq.isEmpty()) {
-        //     System.out.println(pq.poll().cnt);
-        // }
-        
-        while(!pq.isEmpty()) {
-            Pair polled = pq.poll();
-            // System.out.println("total : " + total + " , cnt : " + polled.cnt);
-            if(total - polled.cnt <= k) {
-                // pq.add(polled);
-                if(total - polled.cnt < k) pq.add(polled);
-                break;
-            }
-            else {
-                total -= polled.cnt;
-            }
-        }        
-        
-        answer = pq.size();
+        answer = solve(tangerine.length, k);
         
         return answer;
     }
+    
+    public void init(int[] tangerine) {
+        cntMap = new HashMap<>();
+        for(int n : tangerine) {
+            int cnt = cntMap.getOrDefault(n, 0);
+            cntMap.put(n, cnt+1);
+        }
+        pq = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
+        for(int size : cntMap.keySet()) {
+            pq.add(new Tangerine(size, cntMap.get(size)));
+        }
+    }
+    
+    public int solve(int total, int k) {
+        while(total > k) {
+            total -= pq.poll().cnt;
+        }
+        if(total < k) {
+            return pq.size()+1;
+        }
+        return pq.size();
+    }
 }
 
-class Pair {
-    int num;
+class Tangerine {
+    int size;
     int cnt;
-    public Pair(int num, int cnt) {
-        this.num = num;
+    public Tangerine(int size, int cnt) {
+        this.size = size;
         this.cnt = cnt;
     }
 }
