317f411c4190df04a1fe109656788b549c89d317
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-02-21 19:57:04 +0900

    [Gold IV] Title: 미로만들기, Time: 140 ms, Memory: 14656 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int[] dx = {1, 0, -1, 0};
+    static final int[] dy = {0, 1, 0, -1};
+    int n;
+    int[][] map;
+    boolean[][] vis;
+    int[][] dp;
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+
+    public void solution() {
+        input();
+        bfs();
+        int answer = dp[n-1][n-1];
+        System.out.println(answer);
+//        printArr(dp);
+    }
+
+    public void input() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            n = Integer.parseInt(br.readLine());
+            map = new int[n][n];
+            dp = new int[n][n];
+            for(int i=0; i<n; i++) {
+                Arrays.fill(dp[i], Integer.MAX_VALUE);
+            }
+            vis = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                String str = br.readLine();
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = str.charAt(j) - '0';
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void bfs() {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0, 0));
+        dp[0][0] = 0;
+        while(!q.isEmpty()) {
+            Point polled = q.poll();
+            int x = polled.x;
+            int y = polled.y;
+//            System.out.println("x : " + x + ", y : " + y);
+            for(int dir=0; dir<4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if(!isInner(nx, ny)) continue;
+                if(map[nx][ny] == 0) {
+                    if(dp[x][y]+1 < dp[nx][ny]) {
+                        dp[nx][ny] = dp[x][y] + 1;
+                        q.add(new Point(nx, ny));
+                    }
+                } else {
+                    if(dp[x][y] < dp[nx][ny]) {
+                        dp[nx][ny] = dp[x][y];
+                        q.add(new Point(nx, ny));
+                    }
+                }
+//                if(dp[nx][ny] > dp[x][y]) {
+//                    if(map[nx][ny] == 0) {
+//                        dp[nx][ny] = dp[x][y] + 1;
+//                    } else {
+//                        dp[nx][ny] = dp[x][y];
+//                    }
+//                    q.add(new Point(nx, ny));
+//                }
+            }
+        }
+    }
+
+    public boolean isInner(int x, int y) {
+        return 0<=x && 0<=y && x<n && y<n;
+    }
+
+    public void printArr(int[][] arr) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Point {
+    int x;
+    int y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
