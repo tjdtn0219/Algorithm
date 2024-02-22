f7a17ccb229ba028ef47c3d8557fca5c56ea0157
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-02-22 17:49:19 +0900

    [level 2] Title: 미로 탈출, Time: 0.33 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -2,78 +2,74 @@ import java.util.*;
 
 class Solution {
     
-    // public char[][] map;
-    public int n, m;
-    public int[][] dis;
-    public int[] dx = {1,0,-1,0};
-    public int[] dy = {0,-1,0,1};
-    // public Queue<Pair> q = new LinkedList<>();
+    static final int[] dx = {1,0,-1,0};
+    static final int[] dy = {0,1,0,-1};
+    
+    int n, m;
+    char[][] map;
+    Point start;
+    Point lever;
+    Point end;
     
     public int solution(String[] maps) {
         int answer = 0;
-        
-        n = maps.length;
-        m = maps[0].length();
-        
-        int si = 0; int sj = 0;
-        int li = 0; int lj = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(maps[i].charAt(j)=='S') {
-                    si = i; sj = j;
-                }
-                if(maps[i].charAt(j)=='L') {
-                    li = i; lj = j;
-                }
-            }
-        }
-        
-        
-        dis = new int[n][m];        
-        int toLever = bfs(si, sj, maps, 'L');
-        if(toLever==-1) return -1;
-        
-        answer += toLever;
-        
-        System.out.println("Lever : " + toLever);
-        
-        dis = new int[n][m];
-        int leverToEnd = bfs(li, lj, maps, 'E');
+        init(maps);
+        int startToLever = bfs(start, lever);
+        if(startToLever == -1) return -1;
+        int leverToEnd = bfs(lever, end);
         if(leverToEnd == -1) return -1;
-        
-        answer += leverToEnd;
-        
-        
+        answer = startToLever + leverToEnd;
         return answer;
     }
     
-    public int bfs(int i, int j, String[] maps, char tg) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
-        dis[i][j] = 1;
-        while(!q.isEmpty()) {
-            Pair polled = q.poll();
-            int x = polled.x;
-            int y = polled.y;
-            for(int dir=0; dir<4; dir++) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-                if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-                if(dis[nx][ny]>0 || maps[nx].charAt(ny)=='X') continue;
-                if(maps[nx].charAt(ny)==tg) return dis[x][y];
-                q.add(new Pair(nx, ny));
-                dis[nx][ny] = dis[x][y] + 1;
+    public void init(String[] maps) {
+        n = maps.length;
+        m = maps[0].length();
+        map = new char[n][m];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                map[i][j] = maps[i].charAt(j);
+                if(map[i][j] == 'S') start = new Point(i, j);
+                if(map[i][j] == 'L') lever = new Point(i, j);
+                if(map[i][j] == 'E') end =new Point(i, j);
             }
         }
-        
+    }
+    
+    public int bfs(Point sp, Point ep) {
+        Queue<Point> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        q.add(sp);
+        vis[sp.x][sp.y] = true;
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                Point cur = q.poll();
+                if(cur.x == ep.x && cur.y == ep.y) return cnt;
+                for(int dir=0; dir<4; dir++) {
+                    int nx = cur.x + dx[dir];
+                    int ny = cur.y + dy[dir];
+                    if(!isInner(nx, ny)) continue;
+                    if(vis[nx][ny] || map[nx][ny] == 'X') continue;
+                    q.add(new Point(nx, ny));
+                    vis[nx][ny] = true;
+                }
+            }
+            cnt++;
+        }
         return -1;
+    }
+    
+    public boolean isInner(int x, int y) {
+        return 0<=x && 0<=y && x<n && y<m;
     }
 }
 
-class Pair {
+class Point {
     int x;
     int y;
-    public Pair(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
