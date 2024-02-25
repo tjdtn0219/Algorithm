0fce0a80bfd109a565d2e24433c64d09706d0595
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-02-25 22:54:23 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 10.89 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -2,58 +2,86 @@ import java.util.*;
 
 class Solution {
     
-    public int[] parent;
-    public boolean[] isCycle;
-    public boolean[] vis;
+    static final int MAX_LEN = 101;
+    
+    int n;
+    int[] boxes;
+    int[] parents;
+    boolean[] isOpened;
     
     public int solution(int[] cards) {
         int answer = 0;
-        
-        int n = cards.length;
-        parent = new int[n+1];
-        isCycle = new boolean[n+1];
-        vis = new boolean[n+1];
-
-       for(int x=1; x<=n; x++) parent[x] = x;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->o2-o1);
-        for(int i=1; i<=n; i++) {
-            if(vis[i]) pq.add(0);
-            else pq.add(getTeam(i, cards));
-        }
-        
-        int cnt1 = pq.poll();
-        int cnt2 = pq.poll();
-        
-        if(cnt1==n) return 0;
-        
-        // System.out.println(cnt1 + " , " + cnt2);
-        answer = cnt1 * cnt2;
-        return answer;
+        init(cards);
+        // int k = dfs(1, 0);
+        // System.out.println("k : " + k);
+        return solve();
+        // return answer;
     }
     
-    public int getTeam(int x, int[] cards) {
-        int cnt = 1;
-        while(true) {
-            int nxt = cards[x-1];
-            if(find(nxt) == find(x) || vis[x]) break;
-            vis[x] = true;
-            union(x, nxt);
-            x = nxt;
-            cnt++;
+    public void init(int[] cards) {
+        n = cards.length;
+        boxes = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            boxes[i] = cards[i-1];
         }
-        return cnt;
+        parents = new int[MAX_LEN];
+        for(int i=1; i<MAX_LEN; i++) {
+            parents[i] = i;
+        }
+        isOpened = new boolean[n+1];
+        
+    }
+    
+    public int solve() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for(int i=1; i<=n; i++) {
+            if(isOpened[i]) continue;
+            int cnt = dfs(i, 0);
+            // System.out.println("idx : " + i + " k : " + k);
+            // printIsOpened();
+            pq.add(cnt);
+        }
+        // System.out.println("size : " + pq.size());
+        if(pq.size()==1) return 0;
+        else {
+            int n1 = pq.poll();
+            int n2 = pq.poll();
+            System.out.println("n1 : " + n1 + " n2 : " + n2);
+            return n1*n2;
+        }
+    }
+    
+    public void printIsOpened() {
+        for(int i=1; i<=n; i++) {
+            System.out.print(isOpened[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public int dfs(int idx, int k) {
+        // System.out.println("card : " + card + " isOpened[card] : " + isOpened[card]);
+        if(isOpened[idx]) {
+            
+            return k;
+        }
+        isOpened[idx] = true;
+        int nxtIdx = boxes[idx];
+        return dfs(nxtIdx, k+1);
+        
     }
     
     public int find(int x) {
-        if(x==parent[x]) return x;
-        else return parent[x] = find(parent[x]);
+        if(parents[x] == x) return x;
+        return parents[x] = find(parents[x]);
     }
     
     public void union(int u, int v) {
         u = find(u);
         v = find(v);
-        if(u<v) parent[v] = u;
-        else parent[u] = v;
-    }
+        if(u < v) {
+            parents[v] = u;
+        } else {
+            parents[u] = v;
+        }
+    } 
 }
