750eaddb967b61f09e9b25a2b65e2f7dbcff6a3d
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-03-12 10:26:07 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,42 +1,51 @@
 import java.util.*;
 
 class Solution {
+    
+    static final int DAYS = 10;
+    
+    HashMap<String, Integer> wantMap;
+    String[] discount;
+    
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
         
-        HashMap<String, Integer> wantMap = new HashMap<>();
+        init(want, number, discount);
+        return solve();
+        // int answer = 0;
+        // return answer;
+    }
+    
+    public void init(String[] want, int[] number, String[] discount) {
+        wantMap = new HashMap<>();
         for(int i=0; i<want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
-        
-        // for(String s : wantMap.keySet()) {
-        //     System.out.println(s + " " + wantMap.get(s));
-        // }
-        
-        for(int i=0; i+9<discount.length; i++) {
-            HashMap<String, Integer> dcMap = new HashMap<>();
-            for(int j=i; j<i+10; j++) {
-                int cnt = dcMap.getOrDefault(discount[j], 0);
-                dcMap.put(discount[j], cnt+1);
-                // System.out.println(j + " " + discount[j] + " " + (cnt+1));
+        this.discount = discount;
+    }
+    
+    public int solve() {
+        int answer = 0;
+        int discountDays = discount.length;
+        for(int i=0; i+DAYS-1<discountDays; i++) {
+            // System.out.println("i : " + isEnableBuy(i, i+DAYS-1));
+            if(isEnableBuy(i, i+DAYS-1)) {
+                answer++;
             }
-            
-            // System.out.println("===========");
-            
-            // for(String s : dcMap.keySet()) {
-            //     System.out.println(s + " " + dcMap.get(s));
-            // }
-            
-            boolean flag = false;
-            for(String item : wantMap.keySet()) {
-                if(wantMap.get(item) > dcMap.getOrDefault(item, 0)) {
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag) answer++;
         }
-        
         return answer;
+    }
+    
+    public boolean isEnableBuy(int st, int en) {
+        HashMap<String, Integer> buyMap = new HashMap<>();
+        for(int i=st; i<=en; i++) {
+            String item = discount[i];
+            buyMap.put(item, buyMap.getOrDefault(item, 0)+1);
+        }
+        for(String item : wantMap.keySet()) {
+            if(wantMap.getOrDefault(item, 0) > buyMap.getOrDefault(item, 0)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
