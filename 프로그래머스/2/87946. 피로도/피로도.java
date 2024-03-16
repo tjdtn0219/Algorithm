3c411ee3c59f049cee215d5b438cdbe78b08f062
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-03-16 14:05:04 +0900

    [level 2] Title: 피로도, Time: 0.10 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,69 +2,36 @@ import java.util.*;
 
 class Solution {
     
-    int[][] dungeons;
-    int n;
-    int energy;
-    int[] comb;
-    boolean[] vis;
-    int answer;
+    public int n;
+    public int[] comb;
+    public boolean[] vis;
+    public int ans = -1;
     
-    public int solution(int k, int[][] dungeons) {
+    public int solution(int cur, int[][] dungeons) {
         
-        init(k, dungeons);
-        solve();
+        n = dungeons.length;    
+        comb = new int[n];
+        vis = new boolean[n];
         
-        return answer;
+        btk(0, cur, dungeons, 0);
+        
+        return ans;
     }
     
-    public void init(int k, int[][] dungeons) {
-        this.dungeons = dungeons;
-        this.n = dungeons.length;
-        this.energy = k;
-        this.comb = new int[n];
-        this.vis = new boolean[n];
-        this.answer = -1;
-    }
-    
-    public void solve() {
-        makeComb(0);
-    }
-    
-    public void makeComb(int k) {
-        if(k == n) {
-            // printComb();
-            answer = Math.max(answer, getClearedDungeonCnt());
+    public void btk(int k, int cur, int[][] dungeons, int cnt) {
+        if(k==n) {
+            ans = Math.max(ans, cnt);
             return ;
         }
         
         for(int i=0; i<n; i++) {
             if(vis[i]) continue;
+            
             comb[k] = i;
             vis[i] = true;
-            makeComb(k+1);
+            if(cur < dungeons[i][0]) btk(k+1, cur, dungeons, cnt);
+            else btk(k+1, cur-dungeons[i][1], dungeons, cnt+1);
             vis[i] = false;
         }
-        
     }
-    
-    public int getClearedDungeonCnt() {
-        int cnt = 0;
-        int curEnergy = energy;
-        for(int idx : comb) {
-            if(curEnergy < dungeons[idx][0]) break;
-            curEnergy -= dungeons[idx][1];
-            cnt++;
-        }
-        return cnt;
-    }
-    
-    public void printComb() {
-        StringBuilder sb = new StringBuilder();
-        for(int num : comb) {
-            sb.append(num).append(" ");
-        }
-        System.out.println(sb);
-    }
-    
-    
 }
