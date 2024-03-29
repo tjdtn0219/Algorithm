2d464c068bab051b0260455d024c608695648192
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-03-29 15:36:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.34 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,42 +1,69 @@
 import java.util.*;
 
 class Solution {
+    
+    Deque<Character> dq;
+    
     public int solution(String s) {
-        int answer = 0;
-        
-        String tmp = s;
-        
-        for(int i=0; i<s.length(); i++) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(tmp.substring(i)).append(tmp.substring(0,i));
-            // System.out.println(sb);
-            if(isCorrect(sb.toString())) answer++;
-        }
-        
-        return answer;
+        init(s);
+        // init("()[{}]");
+        return solve();
     }
     
-    public boolean isCorrect(String s) {
-        Stack<Character> stk = new Stack<>();
-        
+    public void init(String s) {
+        dq = new LinkedList<>();        
         for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
+            // System.out.println(s.charAt(i));
+            dq.addLast(s.charAt(i));
+        }
+    }
+    
+    public void printDq() {
+        StringBuilder sb = new StringBuilder();
+        for(Character c : dq) {
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+    
+    public int solve() {
+        int cnt = 0;
+        // System.out.println("isRight : " + isRight());
+        
+        // System.out.println("size : " + dq.size());
+        for(int i=0; i<dq.size(); i++) {
+            // printDq();
+            if(isRight()) cnt++;
+            rotate();
+        }
+        return cnt;
+    }
+    
+    public boolean isRight() {
+        Stack<Character> stk = new Stack();
+        for(Character c : dq) {
             if(stk.isEmpty()) {
                 stk.add(c);
-                continue;
-            }
-            if(c=='{' || c=='[' || c=='(') {
-                stk.add(c); continue;
-            }
-            if((stk.peek()=='[' && c==']') || (stk.peek()=='(' && c==')') || (stk.peek()=='{' && c=='}')) {
-                stk.pop();
-            } else {
-                // System.out.println("TAG : " + stk.peek() + " , " + c);
-                return false;
+            } else{
+                if(isPair(stk.peek(), c)) {
+                    stk.pop();
+                } else {
+                    stk.add(c);
+                }
             }
         }
-        
-        if(stk.isEmpty()) return true;
-        else return false;
+        if(!stk.isEmpty()) return false;
+        return true;
+    }
+    
+    public boolean isPair(char c1, char c2) {
+        if(c1 == '[' && c2 == ']') return true;
+        if(c1 == '{' && c2 == '}') return true;
+        if(c1 == '(' && c2 == ')') return true;
+        return false;
+    }
+    
+    public void rotate() {
+        dq.addLast(dq.removeFirst());
     }
 }
