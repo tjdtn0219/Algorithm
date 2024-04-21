afdb45805b4e946b1f210041a3e91abfae707a16
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-04-21 17:27:31 +0900

    [Gold V] Title: 감소하는 수, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,42 +1,57 @@
-import java.util.*;
 import java.io.*;
+        import java.util.*;
 
 public class Main {
-    static List<Long> list = new ArrayList<>();
-    static int N;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final int MAX = 1_000_000;
+    static final int[] NUMS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-//        {0,1,2,3,4,5,6,7,8,9}의 감소하는 수의 경우의 수는 총 부분집합의 개수와 같다.
-//        예를 들어 {1,5,9,8}의 부분집합에서 감사하는 수는 1개만 존재하기 때문이다.
-        N = Integer.parseInt(br.readLine());
-        if(N <= 10) {
-            System.out.print(N);
-            return;
-        } else if (N >= 1023) {
-            System.out.print(-1);
-            return;
-        }
+    int n;
+    List<Long> answerList;
 
-        for(int i = 0; i < 10; i++) {
-            solution(i);
-        }
-
-        Collections.sort(list);
-        System.out.print(list.get(N));
+    public static void main(String[] args) {
+        new Main().solution();
     }
 
-    public static void solution(long num) {
-        list.add(num);
-        long modValue = num % 10;
-        if (modValue == 0) {
-            return;
+    public void solution() {
+        input();
+        solve();
+    }
+
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            answerList = new ArrayList<>();
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!");
+            throw new RuntimeException(e);
         }
 
-        for (long i = modValue - 1; i >= 0; i--) {
-            long newValue = num * 10 + i;
-            solution(newValue);
+    }
+
+    public void solve() {
+        fillAnswerList();
+        Collections.sort(answerList);
+        if(n >= answerList.size()) System.out.println(-1);
+        else System.out.println(answerList.get(n));
+    }
+
+
+    public void fillAnswerList() {
+        for(int i=0; i<10; i++) {
+            dfs(i);
         }
+    }
+
+    public void dfs(long num) {
+        answerList.add(num);
+        int mod = (int) (num%10);
+
+        for(int i=mod-1; i>=0; i--) {
+            long nxt = num*10 + i;
+            dfs(nxt);
+        }
+
     }
 }
