e93e3e08e2fdc0e335b44f524ef100763666b551
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-05-01 18:27:27 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2796 ms, Memory: 320584 KB -BaekjoonHub

--- 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -1,37 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main
-{
-	
-	public static void main(String args[]) throws Exception {
-	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
 
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t=0; t<T; t++) {
-			int k = Integer.parseInt(br.readLine());
-			PriorityQueue<Long> pq = new PriorityQueue<>();
-			String[] strings = br.readLine().split(" ");
-			for(int i=0; i<k; i++) {
-				pq.add(Long.parseLong(strings[i]));
-			}
-			
-			long ans = 0;
-			while(pq.size() > 1) {
-				long n1 = pq.poll();
-				long n2 = pq.poll();
-				ans += n1 + n2;
-				pq.add(n1+n2);
-			}
-			
-			System.out.println(ans);
-			
-		}
-		
-	}
-   
+    int n;
+    int[] files;
+    PriorityQueue<Long> pq;
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+
+    public void solution() {
+        input();
+    }
+
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int t = Integer.parseInt(br.readLine());
+            for(int i=0; i<t; i++) {
+                n = Integer.parseInt(br.readLine());
+                files = new int[n];
+                files = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                pq = new PriorityQueue<>((o1, o2) -> (int) (o1 - o2));
+                initPQ();
+                solve();
+            }
+        } catch (Exception e) {
+            System.out.println("InPUT ERROR!!!");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void initPQ() {
+        for(int file : files) {
+            pq.add((long) file);
+        }
+    }
+
+    public void solve() {
+        long cost = 0;
+        while(pq.size() > 1) {
+            long num1 = pq.poll();
+            long num2 = pq.poll();
+            cost += num1 + num2;
+            pq.add(num1 + num2);
+        }
+        System.out.println(cost);
+    }
 }
-
