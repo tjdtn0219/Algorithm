1340a52026e7044f6f89e8a0d08efdfb76c8f5d8
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-05-18 17:53:36 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 332 ms, Memory: 19032 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,41 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static int n;
-    public static List<Egg> eggList = new ArrayList<>();
-    public static boolean[] broken;
-    public static int ans = 0;
-    public static int cnt = 0;
+    int n;
+    List<Egg> eggList;
+    boolean[] broken;
+    int cnt;
+    int answer;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        broken = new boolean[n];
-        for(int i=0; i<n; i++) {
-            String[] strings = br.readLine().split(" ");
-            int s = Integer.parseInt(strings[0]);
-            int w = Integer.parseInt(strings[1]);
-            eggList.add(new Egg(s, w));
-        }
-
-        btk(0);
-
-        System.out.println(ans);
-
+    public static void main(String[] args) {
+        new Main().solution();
     }
 
-    public static void btk(int k) {
+    public void solution() {
+        input();
+        solve();
+    }
+
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            eggList = new ArrayList<>();
+            for(int i=0; i<n; i++) {
+                String[] tmp = br.readLine().split(" ");
+                int s = Integer.parseInt(tmp[0]);
+                int w = Integer.parseInt(tmp[1]);
+                eggList.add(new Egg(s, w));
+            }
+            broken = new boolean[n];
+            cnt = 0;
+            answer = 0;
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void solve() {
+        dfs(0);
+        System.out.println(answer);
+    }
+
+    public void dfs(int k) {
 
         if(k==n) {
-            ans = Math.max(ans, cnt);
+            answer = Math.max(answer, cnt);
             return ;
         }
 
         if(broken[k] || cnt==n-1) {
-            btk(k+1);
+            dfs(k+1);
             return;
         }
 
@@ -51,7 +67,7 @@ public class Main {
                     broken[i] = true;
                     cnt++;
                 }
-                btk(k+1);
+                dfs(k+1);
                 rollBack(eggList.get(k), eggList.get(i));
                 if(broken[k]) {
                     broken[k] = false;
@@ -86,3 +102,59 @@ class Egg {
         this.w = w;
     }
 }
+
+
+//    public void dfs(int curIdx) {
+////        System.out.println("curIdx : " + curIdx);
+//
+//        if(curIdx == n) {
+//            int cnt = 0;
+//            for(int i=0; i<n; i++) {
+//                if(isBroken[i]) cnt++;
+//            }
+////            System.out.println("cnt : " + cnt);
+//            answer = Math.max(answer, cnt);
+//            return ;
+//        }
+//
+//        if(isBroken[curIdx]) {
+//            dfs(curIdx + 1);
+//        }
+//
+//        for(int i=0; i<n; i++) {
+//            if(curIdx == i) continue;
+//            if(isBroken[i]) continue;
+////            System.out.println("i : " + i);
+//            crush(eggList.get(curIdx), eggList.get(i));
+//            dfs(curIdx+1);
+//            rollback(eggList.get(curIdx), eggList.get(i));
+//        }
+//    }
+//
+//    public void crush(Egg egg1, Egg egg2) {
+//        egg1.s -= egg2.w;
+//        egg2.s -= egg1.w;
+//        if(egg1.s <= 0) isBroken[egg1.idx] = true;
+//        if(egg2.s <= 0) isBroken[egg2.idx] = true;
+//    }
+//
+//    public void rollback(Egg egg1, Egg egg2) {
+//        egg1.s += egg2.w;
+//        egg2.s += egg1.w;
+//        if(egg1.s > 0) isBroken[egg1.idx] = false;
+//        if(egg2.s > 0) isBroken[egg2.idx] = false;
+//    }
+//
+//}
+//
+//class Egg {
+//    int idx;
+//    int s;
+//    int w;
+//
+//    public Egg(int idx, int s, int w) {
+//        this.idx = idx;
+//        this.s = s;
+//        this.w = w;
+//    }
+//}
