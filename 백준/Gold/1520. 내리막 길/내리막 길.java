b766218e541cb1bbe06820c909f941c9ced7f537
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-06-09 00:47:39 +0900

    [Gold III] Title: 내리막 길, Time: 392 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
 
-    public static final int[] DX = {1,0,-1,0};  //남, 동, 북, 서
-    public static final int[] DY = {0,1,0,-1};
+    static final int[] DX = {1,0,-1,0};
+    static final int[] DY = {0,1,0,-1};
 
     int n, m;
     int[][] map;
@@ -29,45 +29,51 @@ public class Main {
             map = new int[n][m];
             dp = new int[n][m];
             for(int i=0; i<n; i++) {
-                tmp = br.readLine().split(" ");
-                for(int j=0; j<m; j++) {
-                    map[i][j] = Integer.parseInt(tmp[j]);
-                    dp[i][j] = -1;
-                }
+                map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                Arrays.fill(dp[i], -1);
             }
             answer = 0;
-        } catch(Exception e) {
-            System.out.println("INPUT ERROR!!!");
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!!!!");
             throw new RuntimeException(e);
         }
-
     }
 
     public void solve() {
+//        printMap();
         dfs(0, 0);
         System.out.println(dp[0][0]);
     }
 
+    public void printMap() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
     public int dfs(int x, int y) {
-        if(x==n-1 && y==m-1) {
+        if(x == n-1 && y == m-1) {
             return 1;
         }
 
         if(dp[x][y] > -1) {
-            //이미 방문했을 때
             return dp[x][y];
         }
 
-        //처음 방문했을 때
         int cnt = 0;
         for(int dir=0; dir<4; dir++) {
             int nx = x + DX[dir];
             int ny = y + DY[dir];
             if(!isInner(nx, ny)) continue;
-            if(map[x][y] > map[nx][ny]) {
-                cnt += dfs(nx, ny);
-            }
+            if(map[x][y] <= map[nx][ny]) continue;
+            cnt += dfs(nx, ny);
         }
+
         dp[x][y] = cnt;
         return dp[x][y];
     }
@@ -76,14 +82,4 @@ public class Main {
         return 0<=x && 0<=y && x<n && y<m;
     }
 
-
-}
-
-class Point {
-    int x;
-    int y;
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
 }
