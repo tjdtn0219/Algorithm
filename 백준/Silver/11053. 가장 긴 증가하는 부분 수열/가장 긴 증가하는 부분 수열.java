7f18064687fa1886e51a206a447c390d2da8a123
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-06-15 19:17:46 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,85 +5,69 @@ public class Main {
 
     int n;
     int[] arr;
-    int[] dp;
     List<Integer> stk;
 
     public static void main(String[] args) {
         new Main().solution();
-
     }
 
     public void solution() {
         input();
-        System.out.println(solve());
+        solve();
     }
 
     public void input() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             n = Integer.parseInt(br.readLine());
-            arr = new int[n];
-            dp = new int[n];
-            Arrays.fill(dp, 1);
-//            for(int i=0; i<n; i++) {
-//                arr[i] = Integer.parseInt(br.readLine());
-//            }
-            String[] tmp = br.readLine().split(" ");
-            for(int i=0; i<n; i++) {
-                arr[i] = Integer.parseInt(tmp[i]);
-            }
+            arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             stk = new ArrayList<>();
         } catch (Exception e) {
-            System.out.println("INPUT ERROR!");
+            System.out.println("INPUT ERROR!!!!!!");
             throw new RuntimeException(e);
         }
     }
 
-    public int solve() {
-//        return LTS();
-        return LTS_DP();
+    public void solve() {
+        System.out.println(LTS());
     }
 
     public int LTS() {
         for(int num : arr) {
+//            System.out.println("num : " + num);
+//            printStk();
             if(stk.isEmpty()) {
                 stk.add(num);
             } else if(stk.get(stk.size()-1) < num) {
                 stk.add(num);
             } else {
-                int idx = lower_idx(num);
-                //size보다 클 경우
-                if(stk.size() == idx) stk.add(num);
+                int idx = lowerIdx(num);
+                if(idx == stk.size()) stk.add(num);
                 else stk.set(idx, num);
             }
         }
         return stk.size();
     }
 
-    public int LTS_DP() {
-        for(int i=1; i<arr.length; i++) {
-            for(int j=0; j<i; j++) {
-                if(arr[j] < arr[i]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
-            }
+    public void printStk() {
+        StringBuilder sb = new StringBuilder();
+        for(int num : stk) {
+            sb.append(num + " ");
         }
-        int result = 0;
-        for(int i=0; i<dp.length; i++) {
-            result = Math.max(result, dp[i]);
-        }
-        return result;
+        System.out.println(sb);
     }
-
-    public int lower_idx (int tg) {
+    private int lowerIdx(int tg) {
         int st = 0;
         int en = stk.size();
 
         while(st < en) {
             int mid = (st + en) / 2;
-            if(tg <= arr[mid]) en = mid;
+            if(tg <= stk.get(mid)) en = mid;
+//            if(tg <= arr[mid]) en = mid;
             else st = mid + 1;
         }
+
         return st;
     }
+
 }
