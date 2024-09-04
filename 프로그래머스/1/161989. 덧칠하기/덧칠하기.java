5a3bc87cd656183fe35dd9d72d09bdfa32204e62
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-04 23:14:35 +0900

    [level 1] Title: 덧칠하기, Time: 7.29 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,28 @@
 import java.util.*;
-import java.lang.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int start = section[0];
-        int end = section[0] + (m-1);
-        int ans = 1;
-            
-        for(int i : section){
-            if(i>=start && i<=end)
-                continue;
-            else{
-                start = i;
-                end = i + (m-1);
-                ans++;
-            }
-            
+        int ans = 0;
+        
+        boolean[] isPaint = new boolean[n+1];
+        Arrays.fill(isPaint, true);
+        
+        for(int num : section) {
+            isPaint[num] = false;
         }
+        
+        for(int num : section) {
+            if(isPaint[num]) continue;
+            boolean flag = false;
+            for(int i=num; i<Math.min(num+m,n+1); i++) {
+                if(!isPaint[i]) {
+                    isPaint[i] = true;
+                    flag = true;
+                }
+            }
+            if(flag) ans++;
+        }
+        
         return ans;
     }
 }
