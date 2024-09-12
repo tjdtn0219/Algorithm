88776ab05a99e407c4b1815f68b881d94d56f21f
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-12 22:37:48 +0900

    [Gold II] Title: 보석 도둑, Time: 1448 ms, Memory: 140760 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,62 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    int N;
+    int K;
+    LinkedList<Jewer> jewerList;
+    int[] bags;
 
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] strings = bf.readLine().split(" ");
+    public static void main(String[] args) {
+        new Main().solution();
+    }
 
-        int n = Integer.parseInt(strings[0]);
-        int k = Integer.parseInt(strings[1]);
+    public void solution() {
+        input();
+        solve();
+    }
 
-        List<Jewel> jList = new ArrayList<>();
-        int[] bags = new int[k];
-
-        for(int i=0; i<n; i++) {
-            strings = bf.readLine().split(" ");
-            int w = Integer.parseInt(strings[0]);
-            int c = Integer.parseInt(strings[1]);
-            jList.add(new Jewel(w, c));
-        }
-
-        for(int i=0; i<k; i++) {
-            bags[i] = Integer.parseInt(bf.readLine());
-        }
-        /* *********입력********* */
-
-        Collections.sort(jList, (o1,o2)->{
-            if(o1.weight == o2.weight) return o1.cost - o2.cost;
-            return o1.weight - o2.weight;
-        });
-        Arrays.sort(bags);
-
-        long ans = 0;
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());    //최대 힙 우선순위 큐
-        //우선순위 큐를 안쓴다면, for문을 돌리면서,
-        // jewel.weight<=bag 에서 jewel.weight > bag인 경계 부분을 찾는 구현이 복잡해짐
-        for(int i=0,j=0; i<k; i++) {
-            while(j<n && jList.get(j).weight <= bags[i]) {
-                pq.add(jList.get(j++).cost);
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] N_K = br.readLine().split(" ");
+            N = Integer.parseInt(N_K[0]);
+            K = Integer.parseInt(N_K[1]);
+            jewerList = new LinkedList<>();
+            for(int i=0; i<N; i++) {
+                String[] m_v = br.readLine().split(" ");
+                int m = Integer.parseInt(m_v[0]);
+                int v = Integer.parseInt(m_v[1]);
+                jewerList.add(new Jewer(m, v));
+            }
+            bags = new int[K];
+            for(int i=0; i<K; i++) {
+                bags[i] = Integer.parseInt(br.readLine());
             }
 
-            if(!pq.isEmpty()) ans += pq.poll();
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void solve() {
+        Arrays.sort(bags);
+        Collections.sort(jewerList, (o1, o2) -> {
+            if(o1.m == o2.m) {
+                return o2.v - o1.v;
+            }
+            return o1.m - o2.m;
+        });
+
+        PriorityQueue<Jewer> pq = new PriorityQueue<>((o1, o2) -> {
+            if(o1.v == o2.v) {
+                return o1.m - o2.m;
+            }
+            return o2.v - o1.v;
+        });
+
+        long ans = 0;
+        for(int weight : bags) {
+            while(!jewerList.isEmpty() && jewerList.peek().m <= weight){
+                pq.add(jewerList.pollFirst());
+            }
+
+            if(pq.isEmpty()) continue;
+            ans += pq.poll().v;
         }
 
         System.out.println(ans);
-   }
+    }
+
 }
 
-class Jewel {
-    int weight;
-    int cost;
-
-    public Jewel(int w, int c) {
-        this.weight = w;
-        this.cost = c;
+class Jewer {
+    int m;
+    int v;
+    public Jewer(int m, int v) {
+        this.m = m;
+        this.v = v;
     }
 }
