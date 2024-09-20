15a5de1cf6a25d925ca9ec8097c4043a52c81ca6
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-20 10:42:19 +0900

    [level 2] Title: 광물 캐기, Time: 759.28 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,36 +1,77 @@
-import java.util.*;
-
 class Solution {
     
-    String[] use;
-    int answer;
-    HashMap<String, Integer> hmap = new HashMap<>();
-    String[] weapons;
-    int pickCnt;  //광물을 캔 곡괭이 수
-    String[] weaponComb;
+    static final int LEN = 3;
     
+    int ans;
+    int[] picks;
+    int[] minerals;
+    int[][] tiredArr;   //tiredArr[곡괭이][광물]
+    int[] comb;
+    int n;
     
     public int solution(int[] picks, String[] minerals) {
-        
+        // 0: 돌, 1: 철, 2: 다이아몬드
         init(picks, minerals);
-        // System.out.println("pickCnt : " + pickCnt);
-        makeComb(0, minerals);
-        return answer;
+        // print();
+        makeComb(0, new boolean[n]);
+        return ans;
+    }
+    
+    private void print() {
+        System.out.println("====picks====");
+        for(int p : picks) {
+            System.out.print(p + " ");
+        }
+        System.out.println("\n============");
+    }
+    
+    public void makeComb(int k, boolean[] vis) {
+        if(k == n) {
+            // StringBuilder sb = new StringBuilder();
+            // for(int num : comb) {
+            //     sb.append(num).append(" ");
+            // }
+            // System.out.println(sb);
+            int[] pickArr = getPickArr();
+            mine(pickArr);
+            return ;
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(vis[i]) continue;
+            vis[i] = true;
+            comb[k] = i;
+            makeComb(k+1, vis);
+            vis[i] = false;
+        }
+    }
+    
+    public void mine(int[] pickArr) {
+        int min = Math.min(pickArr.length, minerals.length);
+        int sum = 0;
+        for(int i=0; i<min; i++) {
+            int p = pickArr[i];
+            int m = minerals[i];
+            sum += tiredArr[p][m];
+        }
+        ans = Math.min(ans, sum);
+    }
+    
+    public int[] getPickArr() {
+        int[] arr = new int[n*5];
+        for(int i=0; i<n; i++) {
+            int idx = comb[i];
+            int pick = picks[idx];
+            for(int j=0; j<5; j++) {
+                arr[i*5 + j] = pick;
+            }
+        }
+        return arr;
     }
     
     public void init(int[] picks, String[] minerals) {
-        answer = Integer.MAX_VALUE;
-        hmap = new HashMap<>();
-        hmap.put("diamond", picks[0]);
-        hmap.put("iron", picks[1]);
-        hmap.put("stone", picks[2]);
-        weapons = new String[]{"diamond", "iron", "stone"};
-        pickCnt = getPickCnt(picks, minerals);
-        weaponComb = new String[pickCnt];
-    }
-    
-    public int getPickCnt(int[] picks, String[] minerals) {
-        int n = 0;
+        this.ans = Integer.MAX_VALUE;
+        n = 0;
         int pickSum = 0;
         for(int pick : picks) {
             pickSum += pick;
@@ -42,52 +83,35 @@ class Solution {
             n = mineralCnt/5;
             if(mineralCnt%5 != 0) n++;
         }
-        return n;
-    }
-    
-    public void makeComb(int k, String[] minerals) {
-        if(k == pickCnt) {
-            work(minerals);
-            return ;
-        }
-        
-        for(String weapon : weapons) {
-            if(hmap.get(weapon) == 0) continue;
-            weaponComb[k] = weapon;
-            int cnt = hmap.get(weapon);
-            hmap.put(weapon, cnt-1);
-            makeComb(k+1, minerals);
-            hmap.put(weapon, cnt);
-        }
-    }
-    
-    public void work(String[] minerals) {
-        List<String> weaponList = new ArrayList<>();
-        for(String weapon : weaponComb) {
-            for(int i=0; i<5; i++) {
-                weaponList.add(weapon);
+        System.out.println("n : " + n);
+        this.picks = new int[pickSum];
+        int idx = 0;
+        for(int i=0; i<LEN; i++) {
+            for(int j=0; j<picks[i]; j++) {
+                if(i == 0) this.picks[idx++] = 3;
+                else if(i == 1) this.picks[idx++] = 2;
+                else if(i == 2) this.picks[idx++] = 1;
             }
         }
-        
-        int sum = 0;
-        int minLen = Math.min(minerals.length, weaponList.size());
-        for(int i=0; i<minLen; i++) {
-            sum += getEnergy(minerals[i], weaponList.get(i));
+        this.minerals = new int[minerals.length];
+        for(int i=0; i<minerals.length; i++) {
+            if(minerals[i].equals("diamond")) {
+                this.minerals[i] = 3;
+            } else if(minerals[i].equals("iron")) {
+                this.minerals[i] = 2;
+            } else {
+                this.minerals[i] = 1;   
+            }
         }
-        answer = Math.min(answer, sum);
+        this.tiredArr = new int[LEN+1][LEN+1];
+        for(int i=1; i<=LEN; i++) {
+            for(int j=1; j<=i; j++) {
+                tiredArr[i][j] = 1;
+            }
+        }
+        tiredArr[1][2] = 5;
+        tiredArr[1][3] = 25;
+        tiredArr[2][3] = 5;
+        comb = new int[n];
     }
-    
-    public int getEnergy(String mineral, String weapon) {
-        if(weapon.equals("diamond")) return 1;
-        else if(weapon.equals("iron")) {
-            if(mineral.equals("diamond")) return 5;
-            else return 1;
-        }
-        else {  //stone
-            if(mineral.equals("diamond")) return 25;
-            else if(mineral.equals("iron")) return 5;
-            else return 1;
-        }
-    }
-    
 }
