1d4fef4c77e2bbf6a2185382540b1e7f70418bdd
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-21 12:23:22 +0900

    [Gold IV] Title: 타임머신, Time: 260 ms, Memory: 28000 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -3,77 +3,89 @@ import java.util.*;
 
 public class Main {
 
-//    public static final int INF = 500 * 100000;
-    public static final int INF = Integer.MAX_VALUE;
-//    public static List<List<Pair>> graph = new ArrayList<>();
+    static final int INF = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
+    int n, m;
+    List<List<Edge>> graph;
+    long[] dis;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        new Main().solution();
+    }
 
-        String[] strings = br.readLine().split(" ");
+    public void solution() {
+        input();
+        solve();
+    }
 
-        int n = Integer.parseInt(strings[0]);
-        int m = Integer.parseInt(strings[1]);
-
-        List<Edge> list = new ArrayList<>();
-
-//        int[] dis = new int[n + 1];
-        long[] dis = new long[n + 1];
-        Arrays.fill(dis, INF);
-
-        for (int i = 0; i < m; i++) {
-            strings = br.readLine().split(" ");
-            int a = Integer.parseInt(strings[0]);
-            int b = Integer.parseInt(strings[1]);
-            int c = Integer.parseInt(strings[2]);
-            list.add(new Edge(a,b,c));
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] n_m = br.readLine().split(" ");
+            n = Integer.parseInt(n_m[0]);
+            m = Integer.parseInt(n_m[1]);
+            graph = new ArrayList<>();
+            for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
+            for(int i=0; i<m; i++) {
+                String[] a_b_c = br.readLine().split(" ");
+                int a = Integer.parseInt(a_b_c[0]);
+                int b = Integer.parseInt(a_b_c[1]);
+                int c = Integer.parseInt(a_b_c[2]);
+                graph.get(a).add(new Edge(b, c));
+            }
+            dis = new long[n+1];
+            Arrays.fill(dis, INF);
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!");
+            throw new RuntimeException(e);
         }
+    }
 
+    public void solve() {
         dis[1] = 0;
 
         for(int i=0; i<n-1; i++) {
-            for(int j=0; j<m; j++) {
-                Edge edge = list.get(j);
-                if(dis[edge.a] != INF && dis[edge.b] > dis[edge.a] + edge.c) {
-                    dis[edge.b] = dis[edge.a] + edge.c;
+            for(int j=1; j<=n; j++) {
+                for(Edge edge : graph.get(j)) {
+                    if(dis[j] != INF && dis[edge.dest] > dis[j] + edge.cost) {
+                        dis[edge.dest] = dis[j] + edge.cost;
+
+                    }
                 }
             }
         }
 
         boolean isCycle = false;
 
-        for(int i=0; i<m; i++) {
-            Edge edge = list.get(i);
-            if(dis[edge.a] != INF && dis[edge.b] > dis[edge.a] + edge.c) {
-                isCycle = true;
-                break;
+        for(int i=1; i<=n; i++) {
+            for(Edge edge : graph.get(i)) {
+                if(dis[i] != INF && dis[edge.dest] > dis[i] + edge.cost) {
+                    isCycle = true;
+                }
             }
         }
 
         if(isCycle) {
             System.out.println(-1);
             return ;
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for(int i=2; i<=n; i++) {
-                if(dis[i] == INF) sb.append(-1).append("\n");
-                else sb.append(dis[i]).append("\n");
-            }
-            System.out.println(sb);
         }
+        StringBuilder sb = new StringBuilder();
+        for(int i=2; i<=n; i++) {
+            if(dis[i] == INF) sb.append(-1).append("\n");
+            else sb.append(dis[i]).append("\n");
+        }
+        System.out.println(sb);
 
     }
 
 }
 
 class Edge {
-    int a;
-    int b;
-    int c;
-    public Edge(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    int dest;
+    int cost;
+
+    public Edge(int dest, int cost) {
+        this.dest = dest;
+        this.cost = cost;
     }
 }
