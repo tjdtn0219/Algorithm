d419a3da5d2ae6101b75df59a0ec53cea39b44d0
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-22 11:01:08 +0900

    [Gold V] Title: 트리, Time: 112 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -3,84 +3,78 @@ import java.util.*;
 
 public class Main {
 
-    public static List<List<Integer>> tree;
-    public static boolean[] isDeleted;
-    public static int count = 0;
-    public static int del = 0;
+    static final int INF = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
+    int n;
+    int[] parents;
+    List<List<Integer>> graph;
+    int deleted;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        isDeleted = new boolean[n];
-        tree = new ArrayList<>();
-        int root = 0;
-
-        for(int i=0; i<n; i++) {
-            tree.add(new ArrayList<>());
-        }
-
-        String[] strings = br.readLine().split(" ");
-        for(int child=0; child<n; child++) {
-            int parent = Integer.parseInt(strings[child]);
-            if(parent==-1) root = child;
-            else tree.get(parent).add(child);
-        }
-
-        del = Integer.parseInt(br.readLine());
-
-        delete(del);
-        dfss(root);
-
-        System.out.println(count);
+    public static void main(String[] args) {
+        new Main().solution();
     }
 
-    public static void delete(int cur) {
-        isDeleted[cur] = true;
-        for(int child : tree.get(cur)) {
-            if(!isDeleted[child]) {
-                delete(child);
+    public void solution() {
+        input();
+        solve();
+    }
+
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            parents = new int[n];
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            graph = new ArrayList<>();
+            for(int i=0; i<n; i++) graph.add(new ArrayList<>());
+
+            for(int i=0; i<n; i++) {
+                int parent = arr[i];
+                parents[i] = parent;
+                if(parent >= 0) {
+                    graph.get(parent).add(i);
+                }
             }
+            deleted = Integer.parseInt(br.readLine());
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!");
+            throw new RuntimeException(e);
         }
     }
 
-    public static void dfss(int cur) {
+    public void solve() {
+        int parent = parents[deleted];
+        if(parent >= 0) {
+            int root = getRoot(deleted);
+            // System.out.println("root : " + root);
+            System.out.println(bfs(root));
+        } else {
+            System.out.println(0);
+        }
+    }
+
+    public int bfs(int root) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+
         int cnt = 0;
-        if(isDeleted[cur]) return ;
-
-        for(int child : tree.get(cur)) {
-            if(!isDeleted[child]) {
-                dfss(child);
-                cnt++;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            boolean flag = false;
+            for(int child : graph.get(cur)) {
+                if(child == deleted) continue;
+                q.add(child);
+                flag = true;
             }
+            if(!flag) cnt++;
         }
-        if(!isDeleted[cur] && cnt==0) {
-            count++;
-        }
+        // System.out.println("cnt : " + cnt);
+        return cnt;
     }
 
-//    public static void dfs(int cur) {
-//        int cnt = 0;
-//        if(isDeleted[cur]) return;
-//
-//        for(int child : graph.get(cur)) {
-//            if(!isDeleted[child]) {
-//                dfs(child);
-//                cnt++;
-//            }
-//        }
-//        if(!isDeleted[cur] && cnt==0) {
-//            count++;
-//        }
-//    }
-//
-//    public static void deleteDfs(int cur) {
-//        isDeleted[cur] = true;
-//        for(int child : graph.get(cur)) {
-//            if(!isDeleted[child]) {
-//                deleteDfs(child);
-//            }
-//        }
-//    }
+    public int getRoot(int x) {
+        if(parents[x] == -1) return x;
+        return getRoot(parents[x]);
+    }
 }
