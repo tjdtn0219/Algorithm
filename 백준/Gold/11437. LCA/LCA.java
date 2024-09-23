606ce7d8aa21d858a7f54b97b115bdbf8f3bb097
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-09-23 23:23:56 +0900

    [Gold III] Title: LCA, Time: 1308 ms, Memory: 48320 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -3,101 +3,85 @@ import java.util.*;
 
 public class Main {
 
-    int n, m;
-    int[] parents;
-    List<List<Integer>> graph;
-    int[][] inputs;
-    int[] depth;
+    public static List<List<Integer>> tree = new ArrayList<>();
+    public static int[] parent;
+    public static int[] depth;
 
-    public static void main(String[] args) {
-        new Main().solution();
-    }
+    public static void main(String[] args) throws IOException {
 
-    public void solution() {
-        input();
-        solve();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public void input() {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            n = Integer.parseInt(br.readLine());
-            parents = new int[n+1];
-            graph = new ArrayList<>();
-            for(int i=0; i<=n; i++) {
-                parents[i] = i;
-                graph.add(new ArrayList<>());
-            }
-            for(int i=0; i<n-1; i++) {
-                String[] tmp = br.readLine().split(" ");
-                int u = Integer.parseInt(tmp[0]);
-                int v = Integer.parseInt(tmp[1]);
-                graph.get(u).add(v);
-                graph.get(v).add(u);
-            }
-            m = Integer.parseInt(br.readLine());
-            inputs = new int[m][2];
-            for(int i=0; i<m; i++) {
-                inputs[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            }
-            depth = new int[n+1];
-        } catch (Exception e) {
-            System.out.println("INPUT ERROR!!!!!!");
-            throw new RuntimeException(e);
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<=n; i++) {
+            tree.add(new ArrayList<>());
         }
-    }
+        parent = new int[n+1];
+        depth = new int[n+1];
 
-    public void initParents() {
-        int root = 1;
-        boolean[] vis = new boolean[n+1];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(root);
-        vis[root] = true;
-        int dep = 0;
+        for(int i=1; i<n; i++) {
+            String[] strings = br.readLine().split(" ");
+            int u = Integer.parseInt(strings[0]);
+            int v = Integer.parseInt(strings[1]);
 
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i=0; i<size; i++) {
-                int cur = q.poll();
-                for(int child : graph.get(cur)) {
-                    if(vis[child]) continue;
-                    q.add(child);
-                    parents[child] = cur;
-                    vis[child] = true;
-                    depth[child] = dep+1;
-                }
-            }
-            dep++;
+            tree.get(u).add(v);
+            tree.get(v).add(u);
         }
-    }
 
-    public void solve() {
-        initParents();
-//        printDepth();
-        for(int[] arr : inputs) {
-            int u = arr[0];
-            int v = arr[1];
-            System.out.println(lca(u, v));
+        bfs(1);
+
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++) {
+            String[] strings = br.readLine().split(" ");
+
+            int u = Integer.parseInt(strings[0]);
+            int v = Integer.parseInt(strings[1]);
+
+            sb.append(lca(u, v)).append("\n");
         }
+
+        System.out.println(sb);
+
     }
 
-    public int lca(int u, int v) {
+    public static int lca(int u, int v) {
 
         while(depth[u] > depth[v]) {
-            u = parents[u];
+            u = parent[u];
         }
 
         while(depth[u] < depth[v]) {
-            v = parents[v];
+            v = parent[v];
         }
 
         while(u!=v) {
-            u = parents[u];
-            v = parents[v];
+            u = parent[u];
+            v = parent[v];
         }
 
         return u;
 
     }
 
+    public static void bfs(int root) {
+        int curDep = 1;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        depth[root] = curDep++;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                int polled = q.poll();
+                for(int adj : tree.get(polled)) {
+                    if(depth[adj] > 0) continue;
+                    q.add(adj);
+                    depth[adj] = curDep;
+                    parent[adj] = polled;
+                }
+            }
+            curDep++;
+        }
+    }
 }
