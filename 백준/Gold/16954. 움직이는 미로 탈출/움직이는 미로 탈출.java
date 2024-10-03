5b6c2eabad4edce30b13ae2256de9818b9aa81ca
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-10-03 15:28:44 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 1280 ms, Memory: 826928 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;;
 
 class Node{
 	int x;
