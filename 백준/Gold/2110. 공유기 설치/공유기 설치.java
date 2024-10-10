046642ca19839845fb8f413029a443fed72f8a0b
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-10-10 12:11:34 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 29212 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,12 +1,12 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 
     static final int MAX = 1_000_000_000;
 
-    int n, m;
-    int[] locations;
+    int n, c;
+    int[] arr;
 
     public static void main(String[] args) {
         new Main().solution();
@@ -20,50 +20,56 @@ public class Main {
     public void input() {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String[] tmp = br.readLine().split(" ");
-            n = Integer.parseInt(tmp[0]);
-            m = Integer.parseInt(tmp[1]);
-            locations = new int[n];
+            String[] n_c = br.readLine().split(" ");
+            n = Integer.parseInt(n_c[0]);
+            c = Integer.parseInt(n_c[1]);
+            arr = new int[n];
             for(int i=0; i<n; i++) {
-                locations[i] = Integer.parseInt(br.readLine());
+                arr[i] = Integer.parseInt(br.readLine());
             }
-            Arrays.sort(locations);
+            Arrays.sort(arr);
         } catch (Exception e) {
-            System.out.println("INPUT ERROR!!!!!!");
+            System.out.println("INPUT ERROR!!");
             throw new RuntimeException(e);
         }
     }
 
     public void solve() {
-        bSearch();
-    }
-
-    public void bSearch() {
-        int st = 1;
-        int en = MAX + 1;
-        while(st < en) {
+        int st = 0;
+        int en = MAX;
+        int ans = 0;
+        while(st <= en) {
             int mid = (st + en) / 2;
-            if(canInstall(mid) < m) en = mid;
-            else st = mid + 1;
-        }
-        System.out.println(st - 1);
-    }
-
-
-    public int canInstall(int gap) {
-        int count = 1;
-        int lastLocate = locations[0];
-
-        for(int i = 1; i < n; i++) {
-            int locate = locations[i];
-            if(locate - lastLocate >= gap) {
-                count++;
-                lastLocate = locate;
+            // System.out.println("st : " + st + ", en : " + en + ", mid : " + mid);
+            int cnt = getCanInstallCnt(mid);
+            if(cnt >= c) {
+                ans = mid;
+                st = mid + 1;
+            } else if(cnt < c) {
+                en = mid - 1;
+            } else {
+                ans = Math.max(ans, mid);
+                st = mid + 1;
             }
         }
-        return count;
+
+        System.out.println(ans);
 
     }
 
+    public int getCanInstallCnt(int gap) {
+        int cnt = 1;
+        int last = arr[0];
+
+        for(int i=1; i<n; i++) {
+            int cur = arr[i];
+            if(cur - last >= gap) {
+                cnt++;
+                last = cur;
+            }
+        }
+
+        return cnt;
+    }
 
 }
