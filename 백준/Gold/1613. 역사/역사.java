d705bd8b9a9a7110a2e27b392a370c3181a295f1
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-11-12 16:13:10 +0900

    [Gold III] Title: 역사, Time: 532 ms, Memory: 48412 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -1,67 +1,76 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+
 public class Main {
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
- 
-        boolean[][] arr = new boolean[N + 1][N + 1];
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
- 
-            arr[x][y] = true;
-        }
-        
-        // 플로이드 와샬 알고리즘
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (arr[i][k] && arr[k][j]) {
-                        arr[i][j] = true;
+
+    int n, k, s;
+    int[][] arr;
+    int[][] inArr;
+
+    public static void main(String[] args) {
+        new Main().solution();
+    }
+
+    public void solution() {
+        input();
+        solve();
+    }
+
+    public void solve() {
+        for(int p=1; p<=n; p++) {
+            for(int i=1; i<=n; i++) {
+                for(int j=1; j<=n; j++) {
+                    if(arr[i][p]==1 && arr[p][j] == 1) {
+                        arr[i][j] = 1;
                     }
                 }
             }
         }
-        
-        int S = Integer.parseInt(br.readLine());
         
         StringBuilder sb = new StringBuilder();
-        while(S-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            
-            if(x < 1 || y < 1 || x > N || y > N) { // 주어진 범위에서 벗어날 경우
+        for(int[] in : inArr) {
+            int a = in[0];
+            int b = in[1];
+            if(a < 1 || b < 1 || a > n || b > n) {
                 sb.append("0\n");
-            }else {
-                if(arr[x][y]) { // x에서 y로 갈 수 있다는 뜻은 사건이 먼저 일어났다는 뜻
-                    sb.append("-1\n");
-                }else {
-                    if(arr[y][x]) { // y에서 x로 갈 수 있다는 뜻은 사건이 나중에 일어났다는 뜻
-                        sb.append("1\n");
-                    }else { // x에서 y, y에서 x로 모두 갈 수 없으면 상관 없는 사건 관계임
-                        sb.append("0\n");
-                    }
-                }
+                continue;
+            }
+            if(arr[a][b] == 1) {
+                sb.append(-1).append("\n");
+            } else if(arr[b][a] == 1) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
             }
         }
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
- 
+
+    public void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] n_k = br.readLine().split(" ");
+            n = Integer.parseInt(n_k[0]);
+            k = Integer.parseInt(n_k[1]);
+            arr = new int[n+5][n+5];
+            for(int i=0; i<k; i++) {
+                String[] a_b = br.readLine().split(" ");
+                int a = Integer.parseInt(a_b[0]);
+                int b = Integer.parseInt(a_b[1]);
+                arr[a][b] = 1;
+            }
+            s = Integer.parseInt(br.readLine());
+            inArr = new int[s][2];
+            for(int i=0; i<s; i++) {
+                String[] a_b = br.readLine().split(" ");
+                int a = Integer.parseInt(a_b[0]);
+                int b = Integer.parseInt(a_b[1]);
+                inArr[i][0] = a;
+                inArr[i][1] = b;
+            }
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!");
+            throw new RuntimeException(e);
+        }
+    }
+
 }
