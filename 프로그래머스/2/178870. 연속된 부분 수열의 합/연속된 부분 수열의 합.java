a4edfce14157b787fd77b5fbcb288b25190180f2
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2024-12-02 10:59:56 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 28.14 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,64 +2,50 @@ import java.util.*;
 
 class Solution {
     
-    int[] answer;
-    int[] sumArr;
     int[] arr;
+    int[] sumArr;
+    int n;
+    int k;
+    int[] ans;
+    
     public int[] solution(int[] sequence, int k) {
-        init(sequence);
-        solve(k);
-        return answer;
+        init(sequence, k);
+        solve();
+        return ans;
     }
     
-    public void init(int[] sequence) {
-        answer = new int[2];
-        arr = new int[sequence.length+1];
-        for(int i=0; i<sequence.length; i++) {
-            arr[i+1] = sequence[i];
-        }
-        makeSumArr(sequence);
-    }
-    
-    public void makeSumArr(int[] sequence) {
-        sumArr = new int[sequence.length + 1];
-        for(int i=1; i<sequence.length+1; i++) {
-            sumArr[i] = sumArr[i-1] + arr[i];
-        }
-    }
-    
-    public void solve(int k) {
-        int left = 1;
-        int right = 1;
-        int minLen = Integer.MAX_VALUE;
-        int minLeft = 0;
-        int minRight = 0;
-        while(left <= right && right < arr.length) {
-            int sum = sumArr[right] - sumArr[left-1];
+    public void solve() {
+        int left = 0;
+        int right = 0;
+        int min = Integer.MAX_VALUE;
+        while(left <= right && right < n) {
+            int sum;
+            if(left == 0) sum = sumArr[right];
+            else sum = sumArr[right] - sumArr[left-1];
             if(sum == k) {
-                if(right - left < minLen) {
-                    minLen = right - left;
-                    minLeft = left;
-                    minRight = right;
+                if(right - left < min) {
+                    min = right - left;
+                    ans[0] = left;
+                    ans[1] = right;
                 }
                 right++;
             } else if(sum > k) {
                 left++;
-            } else {    //sum < k
+            } else {
                 right++;
             }
         }
-        answer[0] = minLeft - 1;
-        answer[1] = minRight - 1;
     }
-}
-
-class Pair {
-    int left;
-    int right;
-    int gap;
-    public Pair(int left, int right, int gap) {
-        this.left = left;
-        this.right = right;
-        this.gap = gap;
+    
+    public void init(int[] arr, int k) {
+        this.arr = arr;
+        this.n = arr.length;
+        this.sumArr = new int[n];
+        sumArr[0] = arr[0];
+        for(int i=1; i<n; i++) {
+            sumArr[i] = sumArr[i-1] + arr[i];
+        }
+        this.k = k;
+        this.ans = new int[2];
     }
 }
