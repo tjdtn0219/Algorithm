2334567074c1fc4990f317a8c00cfcbb1a3bd869
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-01-31 16:31:22 +0900

    [level 2] Title: 배달, Time: 4.31 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,69 +1,65 @@
 import java.util.*;
 
+class Node {
+    int b;
+    int c;
+    public Node(int b, int c) {
+        this.b = b;
+        this.c = c;
+    }
+}
+
 class Solution {
     
-    List<List<Edge>> graph;
-    int n, k;
+    int n;
+    int k;
+    List<List<Node>> graph;
     int[] dis;
+    int ans;
     
     public int solution(int N, int[][] road, int K) {
         init(N, road, K);
-        makeMinDistanceFromStart(1);
-        // printDis();
-        return solve();
+        solve();
+        return ans;
     }
     
-    public void printDis() {
+    public void solve() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+            return o1.c - o2.c;
+        });
+        pq.add(new Node(1, 0));
+        dis[1] = 0;
+        
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            if(cur.c > dis[cur.b]) continue;
+            for(Node nxt : graph.get(cur.b)) {
+                if(dis[cur.b] + nxt.c < dis[nxt.b]) {
+                    dis[nxt.b] = dis[cur.b] + nxt.c;
+                    pq.add(new Node(nxt.b, dis[nxt.b]));
+                }
+            }
+        }
         for(int i=1; i<=n; i++) {
-            System.out.println("dis[" + i + "] = " + dis[i]);
+            if(dis[i] <= k) ans++;
         }
     }
     
     public void init(int n, int[][] road, int k) {
         this.n = n;
         this.k = k;
+        this.dis = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
         graph = new ArrayList<>();
         for(int i=0; i<=n; i++) {
             graph.add(new ArrayList<>());
         }
         for(int[] edge : road) {
-            graph.get(edge[0]).add(new Edge(edge[1], edge[2]));
-            graph.get(edge[1]).add(new Edge(edge[0], edge[2]));
+            int a = edge[0];
+            int b = edge[1];
+            int c = edge[2];
+            graph.get(a).add(new Node(b, c));
+            graph.get(b).add(new Node(a, c));
         }
-        dis = new int[n+1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-    }
-    
-    public void makeMinDistanceFromStart(int start) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        dis[start] = 0;
-        pq.add(new Edge(start, dis[start]));
-        while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
-            if(cur.cost > dis[cur.node]) continue;
-            for(Edge nxt : graph.get(cur.node)) {
-                if(nxt.cost + dis[cur.node] < dis[nxt.node]) {
-                    dis[nxt.node] = nxt.cost + dis[cur.node];
-                    pq.add(new Edge(nxt.node, dis[nxt.node]));
-                }
-            }
-        }
-    }
-    
-    public int solve() {
-        int answer = 0;
-        for(int i=1; i<=n; i++) {
-            if(dis[i] <= k) answer++;
-        }
-        return answer;
-    }
-}
-
-class Edge {
-    int node;
-    int cost;
-    public Edge(int node, int cost) {
-        this.node = node;
-        this.cost = cost;
     }
 }
