08730060697edfdc3010cd021d866bd411cbd25d
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-02 13:20:49 +0900

    [level 2] Title: 소수 찾기, Time: 20.02 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,6 +1,61 @@
+import java.util.*;
+
 class Solution {
+    
+    int n;
+    int[] nums;
+    boolean[] vis;
+    HashSet<Integer> set;
+    
     public int solution(String numbers) {
         int answer = 0;
-        return answer;
+        init(numbers);
+        return solve();
+        // return answer;
+    }
+    
+    public int solve() {
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            dfs(i, 0, "");
+        }
+        for(int n : set) {
+            // System.out.println("n : " + n);
+            if(isPrime(n)) ans++;
+        }
+        return ans;
+    }
+    
+    public boolean isPrime(int num) {
+        if(num <= 1) return false;
+        for(int i=2; i<=Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    public void dfs(int limit, int k, String s) {
+        if(k == limit) {
+            // System.out.println("s : " + s);
+            set.add(Integer.parseInt(s));
+            return ;
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(vis[i]) continue;
+            vis[i] = true;
+            dfs(limit, k+1, s + nums[i]);
+            vis[i] = false;
+        }
+    }
+    
+    public void init(String numbers) {
+        this.n = numbers.length();
+        this.nums = new int[n];
+        this.vis = new boolean[n];
+        this.set = new HashSet<>();
+        for(int i=0; i<n; i++) {
+            nums[i] = numbers.charAt(i) - '0';
+        }
     }
 }
