d5b550be9b65a693923582080af1972ad6d7e916
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-03 19:51:13 +0900

    [level 2] Title: 문자열 압축, Time: 1.87 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 68.8 MB, 시간: 0.04 ms
+메모리: 80 MB, 시간: 1.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 14일 22:20:26
+2025년 02월 03일 19:51:08
 
 ### 문제 설명
 
@@ -93,6 +93,8 @@
 이 경우 어떻게 문자열을 잘라도 압축되지 않으므로 가장 짧은 길이는 17이 됩니다. </p>
 
 <hr>
+
+<p>※ 공지 - 2024년 7월 8일 테스트케이스가 추가되었습니다. 기존에 제출한 코드가 통과하지 못할 수도 있습니다.</p>
 
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,77 +3,68 @@ import java.util.*;
 class Solution {
     
     String s;
-    int len;
+    int n;
     
     public int solution(String s) {
         init(s);
         return solve();
-        
-    }
-    
-    public void init(String s) {
-        this.s = s;
-        this.len = s.length();
     }
     
     public int solve() {
-        // System.out.println("TAG : " + getDigitCnt(1));
-        // System.out.println("TAG : " + getDigitCnt(10));
-        // System.out.println("TAG : " + getDigitCnt(15));
-        // System.out.println("TAG : " + getDigitCnt(101));
-        int answer = len;
-        for(int i=1; i<=len/2; i++) {
-            int compressedLen = getCompressedLen(i);
-            // System.out.println("interval : " + i + " , len : " + compressedLen);
-            answer = Math.min(answer, compressedLen);
+        if(n == 1) return 1;
+        int ans = Integer.MAX_VALUE;
+        for(int len=1; len<=n/2; len++) {
+            ans = Math.min(ans, getCompressedLen(len));
         }
-        // System.out.println("interval " + 1 + " : " + getCompressedLen(1));
-        // System.out.println("interval " + 2 + " : " + getCompressedLen(2));
-        // System.out.println("interval " + 3 + " : " + getCompressedLen(3));
-        // System.out.println("interval " + 4 + " : " + getCompressedLen(4));
-        // System.out.println("interval " + 6 + " : " + getCompressedLen(6));
-        return answer;
+        return ans;
     }
     
-    public int getCompressedLen(int interval) {
-        int compressedLen = len;
-        boolean flag = false;
-        int successCnt = 1;
-        int idx = 0;
+    public int getCompressedLen(int len) {
+        int st = 0;
         String last = "";
-        while(idx < len) {
-            String sub = s.substring(idx, Math.min(idx+interval, len));
+        int cnt = 1;
+        int res = n;
+        boolean flag = false;
+        while(st < n) {
+            String sub = s.substring(st, Math.min(st + len, n));
             if(last.equals(sub)) {
                 if(!flag) {
-                    // System.out.println("첫 연속");
-                    compressedLen -= interval;
-                    successCnt = 2;
+                    // 첫 연속
+                    res -= len;
+                    cnt = 2;
                     flag = true;
                 } else {
-                    compressedLen -= interval;
-                    successCnt++;
-                    // System.out.println("진행중인 연속");
+                    res -= len;
+                    cnt++;
                 }
             } else {
+                if(cnt > 1) {
+                    res += getDigitNum(cnt);
+                }
+                cnt = 1;
+                last = sub;
                 flag = false;
-                // System.out.println("successCnt : " + successCnt);
-                if(successCnt > 1) compressedLen += getDigitCnt(successCnt);
-                successCnt = 1;
             }
-            last = sub;
-            idx += interval;
+            st += len;
         }
-        if(successCnt > 1) compressedLen += getDigitCnt(successCnt);
-        // System.out.println("=================");
-        return compressedLen;
-    }
+        if(cnt > 1) {
+            res += getDigitNum(cnt);
+        }
+        return res;
+    }    
     
-    public int getDigitCnt(int num) {
+    public int getDigitNum(int num) {
         int cnt = 0;
         while(num > 0) {
-            cnt++;
             num /= 10;
+            cnt++;
         }
         return cnt;
     }
+    
+    public void init(String s) {
+        this.n = s.length();
+        this.s = s;
+    }
+    
 }
