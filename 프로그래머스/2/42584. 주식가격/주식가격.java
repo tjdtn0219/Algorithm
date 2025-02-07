ae6234fc2d1e72cfde1702297f9f05f6ed376d37
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-07 12:09:27 +0900

    [level 2] Title: 주식가격, Time: 38.19 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,55 +1,51 @@
 import java.util.*;
 
+class Node {
+    int price;
+    int idx;
+    public Node(int price, int idx) {
+        this.price = price;
+        this.idx = idx;
+    }
+}
+
 class Solution {
     
     int n;
     int[] prices;
-    int[] answer;
     
     public int[] solution(int[] prices) {
+        int[] answer = {};
         init(prices);
-        solve();
+        return solve();
+    }
+    
+    public int[] solve() {
+        int[] answer = new int[n];
+        Stack<Node> stk = new Stack<>();
+        for(int i=0; i<n; i++) {
+            int price = prices[i];
+            if(stk.isEmpty()) {
+                stk.push(new Node(price, i));
+            } else {
+                while(!stk.isEmpty() && stk.peek().price > price) {
+                    Node popped = stk.pop();
+                    answer[popped.idx] = i - popped.idx;
+                }
+                stk.push(new Node(price, i));
+            }
+        }
+        
+        while(!stk.isEmpty()) {
+            answer[stk.peek().idx] = (n-1) - stk.peek().idx;
+            stk.pop();
+        }
+        
         return answer;
     }
     
     public void init(int[] prices) {
         this.n = prices.length;
         this.prices = prices;
-        this.answer = new int[n];
-    }
-    
-    public void solve() {
-        Stack<Info> stk = new Stack<>();
-        for(int i=0; i<n; i++) {
-            int price = prices[i];
-            if(stk.isEmpty()) {
-                stk.add(new Info(i, price));
-            } else {
-                if(stk.peek().val <= price) {
-                    stk.add(new Info(i, price));
-                } else {
-                    while(!stk.isEmpty() && stk.peek().val > price) {
-                        Info popped = stk.pop();
-                        answer[popped.idx] = i - popped.idx;    
-                    }
-                    stk.add(new Info(i, price));
-                }
-            }
-        }
-        for(Info info : stk) {
-            // System.out.println("idx : " + info.idx + ", val : " + info.val);
-            answer[info.idx] = (n-1) - info.idx;
-        }
-        
-    }
-}
-
-class Info {
-    int idx;
-    int val;
-    
-    public Info(int idx, int val) {
-        this.idx = idx;
-        this.val = val;
     }
 }
