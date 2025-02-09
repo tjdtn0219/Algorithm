3873bb4c77ce3fe08fb178329d074296d7ddf1b1
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-09 13:35:50 +0900

    [level 2] Title: 타겟 넘버, Time: 46.39 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,43 +1,63 @@
+import java.util.*;
+
 class Solution {
     
-    boolean[] op;   //true + , false -
     int n;
-    int tg;
-    int ans = 0;
+    int[] numbers;
+    int target;
+    boolean[] isPlus;
+    int ans;
     
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        
-        tg = target;
-        
-        n = numbers.length;
-        op = new boolean[n];
-        
-        dfs(0, numbers);
-        answer = ans;
-        
-        return answer;
+        init(numbers, target);
+        solve();
+        return ans;
     }
     
-    public void dfs(int k, int[] numbers) {
-        if(k==n) {
-            func(numbers);
+    public void solve() {
+        dfs(0);   
+    }
+    
+    private void printArr(boolean[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for(boolean tf : arr) {
+            sb.append(tf).append(" ");
+        }
+        System.out.println(sb);
+    }
+    
+    public void dfs(int k) {
+        if(k == n) {
+            // printArr(isPlus);
+            int res = getResult();
+            if(res == target) {
+                ans++;
+            }
             return ;
         }
         
-        op[k] = true;
-        dfs(k+1, numbers);
-        
-        op[k] = false;
-        dfs(k+1, numbers);
-    } 
+        isPlus[k] = true;
+        dfs(k+1);
+        isPlus[k] = false;
+        dfs(k+1);
+    }
     
-    public void func(int[] numbers) {
-        int result = 0;
+    public int getResult() {
+        int sum = 0;
         for(int i=0; i<n; i++) {
-            if(op[i]) result += numbers[i];
-            else result -= numbers[i];
+            if(isPlus[i]) {
+                sum += numbers[i];
+            } else {
+                sum -= numbers[i];   
+            }
         }
-        if(result == tg) ans++;
+        return sum;
+    }
+    
+    public void init(int[] numbers, int target) {
+        this.n = numbers.length;
+        this.isPlus = new boolean[n];
+        this.numbers = numbers;
+        this.target = target;
     }
 }
