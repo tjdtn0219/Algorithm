d8efeeb282f543fa30394d1f835c5153f1cf368f
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-10 17:41:51 +0900

    [level 2] Title: 할인 행사, Time: 49.05 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,50 +2,62 @@ import java.util.*;
 
 class Solution {
     
-    static final int DAYS = 10;
-    
     HashMap<String, Integer> wantMap;
     String[] discount;
+    int ans;
     
     public int solution(String[] want, int[] number, String[] discount) {
-        
         init(want, number, discount);
-        return solve();
-        // int answer = 0;
-        // return answer;
+        solve();
+        return ans;
     }
     
-    public void init(String[] want, int[] number, String[] discount) {
-        wantMap = new HashMap<>();
-        for(int i=0; i<want.length; i++) {
-            wantMap.put(want[i], number[i]);
-        }
-        this.discount = discount;
-    }
-    
-    public int solve() {
-        int answer = 0;
-        int discountDays = discount.length;
-        for(int i=0; i+DAYS-1<discountDays; i++) {
-            // System.out.println("i : " + isEnableBuy(i, i+DAYS-1));
-            if(isEnableBuy(i, i+DAYS-1)) {
-                answer++;
-            }
-        }
-        return answer;
-    }
-    
-    public boolean isEnableBuy(int st, int en) {
+    public void solve() {
+        
         HashMap<String, Integer> buyMap = new HashMap<>();
-        for(int i=st; i<=en; i++) {
+        for(int i=0; i<10; i++) {
             String item = discount[i];
-            buyMap.put(item, buyMap.getOrDefault(item, 0)+1);
+            buyMap.put(item, buyMap.getOrDefault(item, 0) + 1);
         }
-        for(String item : wantMap.keySet()) {
-            if(wantMap.getOrDefault(item, 0) > buyMap.getOrDefault(item, 0)) {
+        int left = 0;
+        int right = 10;
+        for(int i=0; i<discount.length-10; i++) {
+            // System.out.println("curDay : " + i + ", start_item : " + discount[i]);
+            if(isJoin(buyMap)) {
+                // System.out.println("TAG : isJoin = TRUE : " + i);
+                ans++;
+            }
+            // System.out.println("left : " + left + ", right : " + right);
+            String out = discount[left++];
+            String in = discount[right++];
+            // System.out.println(" out : " + out + ", in : " + in);
+            buyMap.put(out, buyMap.getOrDefault(out, 0) - 1);
+            buyMap.put(in, buyMap.getOrDefault(in, 0) + 1);
+        }
+        if(isJoin(buyMap)) {
+            ans++;
+        }
+    }
+    
+    public boolean isJoin(HashMap<String, Integer> buyMap) {
+        for(String want : wantMap.keySet()) {
+            // System.out.println("item : " + want + ", buyMap.get : " + buyMap.getOrDefault(want, 0));
+            if(buyMap.getOrDefault(want, 0) < wantMap.get(want)) {
                 return false;
             }
         }
         return true;
+    }
+    
+    public void init(String[] want, int[] number, String[] discount) {
+        this.wantMap = new HashMap<>();
+        for(int i=0; i<want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        // for(String key : wantMap.keySet()) {
+        //     System.out.println("key : " + key + " , cnt : " + wantMap.get(key));
+        // }
+        // System.out.println("================================");
+        this.discount = discount;
     }
 }
