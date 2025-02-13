8fcfdc3cd0f0f5df9cbf03900b6390da98466425
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-13 14:28:42 +0900

    [level 2] Title: 요격 시스템, Time: 537.29 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -5,33 +5,33 @@ class Solution {
     int[][] targets;
     
     public int solution(int[][] targets) {
+        int answer = 0;
         init(targets);
         return solve();
     }
     
-    public void init(int[][] targets) {
+    public int solve() {
         Arrays.sort(targets, (o1, o2) -> {
-            if(o1[1] == o2[1]) return o1[0] - o2[0];
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
             return o1[1] - o2[1];
         });
-        this.targets = targets;
-    }
-        
-    public int solve() {
-        int preE = 0;
+        int lastE = 0;
         int cnt = 0;
         
         for(int[] tg : targets) {
             int s = tg[0];
             int e = tg[1];
-            if(preE <= s) {
+            if(lastE <= s) {
+                lastE = e;
                 cnt++;
-                preE = e;
             }
         }
-        
         return cnt;
-
     }
-        
+    
+    public void init(int[][] targets) {
+        this.targets = targets;
+    }
 }
