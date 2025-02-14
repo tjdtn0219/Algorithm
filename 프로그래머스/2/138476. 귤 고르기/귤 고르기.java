7f28cba5bf69a623bd8aef354696ea89996ef913
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-14 17:43:15 +0900

    [level 2] Title: 귤 고르기, Time: 81.96 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,47 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    HashMap<Integer, Integer> cntMap;
-    PriorityQueue<Tangerine> pq;
+    int k;
+    int[] tangerines;
+    int answer;
     
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        
-        init(tangerine);
-        
-        answer = solve(tangerine.length, k);
-        
+        init(k, tangerine);
+        solve();
         return answer;
     }
     
-    public void init(int[] tangerine) {
-        cntMap = new HashMap<>();
-        for(int n : tangerine) {
-            int cnt = cntMap.getOrDefault(n, 0);
-            cntMap.put(n, cnt+1);
+    public void solve() {
+        HashMap<Integer, Integer> cntMap = new HashMap<>();
+        for(int num : tangerines) {
+            cntMap.put(num, cntMap.getOrDefault(num, 0) + 1);
         }
-        pq = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
-        for(int size : cntMap.keySet()) {
-            pq.add(new Tangerine(size, cntMap.get(size)));
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for(int key : cntMap.keySet()) {
+            pq.add(cntMap.get(key));
+        }
+        
+        while(!pq.isEmpty()) {
+            int polled = pq.poll();
+            if(polled >= k) {
+                answer++;
+                break;
+            } else {
+                k -= polled;
+                answer++;
+            }
         }
     }
     
-    public int solve(int total, int k) {
-        while(total > k) {
-            total -= pq.poll().cnt;
-        }
-        if(total < k) {
-            return pq.size()+1;
-        }
-        return pq.size();
-    }
-}
-
-class Tangerine {
-    int size;
-    int cnt;
-    public Tangerine(int size, int cnt) {
-        this.size = size;
-        this.cnt = cnt;
+    public void init(int k, int[] tangerines) {
+        this.k = k;
+        this.tangerines = tangerines;
     }
 }
