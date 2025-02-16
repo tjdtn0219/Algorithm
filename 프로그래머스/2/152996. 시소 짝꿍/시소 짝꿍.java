a1c4b07ef015a307932cec8ca81ee0d1b6313895
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-16 12:35:24 +0900

    [level 2] Title: 시소 짝꿍, Time: 30.44 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -2,52 +2,40 @@ import java.util.*;
 
 class Solution {
     
-    int n;
+    static final double[] MULTIS = {1.0, 1.5, 2.0, (4.0/3.0)};
+    
     int[] weights;
-    HashMap<Double, Integer> cntMap;
-    long ans;
+    long answer;
     
     public long solution(int[] weights) {
-        init(n, weights);
+        init(weights);
         solve();
-        return ans;
+        return answer;
     }
     
     public void solve() {
-        Arrays.sort(weights);
-        //100 100 150 150 180 270 360
-        int last = -1;
-        for(int w : weights) {
-            double w1 = w * 1.0;
-            double w2 = w * 1.5;
-            double w3 = w * (4.0/3.0);
-            double w4 = w * (2.0);
-            // System.out.println(w2 + ", " + w3 + ", " + w4);
-            if(w == last) {
-                ans += cntMap.getOrDefault(w2, 0);
-                ans += cntMap.getOrDefault(w3, 0);
-                ans += cntMap.getOrDefault(w4, 0);
-            } else {
-                long sameCnt = cntMap.getOrDefault(w1, 0);
-                if(sameCnt > 1) {
-                    ans += sameCnt * (sameCnt-1) / 2;
+        HashMap<Double, Integer> cntMap = new HashMap<>();
+        for(int weight : weights) {
+            double w = (double) weight; 
+            cntMap.put(w, cntMap.getOrDefault(w, 0) + 1);
+        }
+        
+        for(double w : cntMap.keySet()) {
+            for(double multi : MULTIS) {
+                if(multi == 1.0) {
+                    long cnt = cntMap.get(w);
+                    answer += cnt * (cnt-1) / 2;
+                } else {
+                    double mw = multi * w;
+                    long cnt1 = cntMap.get(w);
+                    long cnt2 = cntMap.getOrDefault(mw, 0);
+                    answer += cnt1 * cnt2;
                 }
-                ans += cntMap.getOrDefault(w2, 0);
-                ans += cntMap.getOrDefault(w3, 0);
-                ans += cntMap.getOrDefault(w4, 0);
             }
-            // System.out.println("w : " + w + ", ans : " + ans + "\n");
-            last = w;
         }
     }
     
-    public void init(int n, int[] weights) {
-        this.n = weights.length;
+    public void init(int[] weights) {
         this.weights = weights;
-        this.cntMap = new HashMap<>();
-        for(int w : weights) {
-            double dw = (double) w;
-            cntMap.put(dw, cntMap.getOrDefault(dw, 0) + 1);
-        }
     }
 }
