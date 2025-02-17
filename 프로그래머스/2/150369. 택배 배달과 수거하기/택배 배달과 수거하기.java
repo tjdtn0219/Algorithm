19a9aeb76a1d972f69e71f16af264d50b361d2bf
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-17 18:16:55 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 1441.08 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,69 +1,95 @@
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int idx;
+    int val;
+    
+    public Node(int idx, int val) {
+        this.idx = idx;
+        this.val = val;
+    }
+    
+    @Override
+    public int compareTo(Node o) {
+        return o.idx - this.idx;
+    }
+}
+
 class Solution {
     
     int cap;
     int n;
-    int[] deliveries;
-    int[] pickups;
-    
+    PriorityQueue<Node> deliveryPq;
+    PriorityQueue<Node> pickupPq;
+    long answer;
     
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = -1;
         init(cap, n, deliveries, pickups);
-        return solve();
+        solve();
+        return answer;
+    }
+    
+    public void solve() {
+        while(!deliveryPq.isEmpty() || !pickupPq.isEmpty()) {
+            int dis = 0;
+            if(!deliveryPq.isEmpty()) {
+                dis = Math.max(dis, deliveryPq.peek().idx);
+            }
+            if(!pickupPq.isEmpty()) {
+                dis = Math.max(dis, pickupPq.peek().idx);
+            }
+            // System.out.println("dis : " + dis);
+            // System.out.println("deliveryPq.len : " + deliveryPq.size() + ", pickupPq.len : " + pickupPq.size());
+            answer += dis * 2;
+            
+            int tmp = cap;
+            // cap만큼 상자를 싣고 배달지까지 가는 길에 상자 배송
+            while(!deliveryPq.isEmpty()) {
+                int idx = deliveryPq.peek().idx;
+                tmp -= deliveryPq.poll().val;
+                if(tmp < 0) {
+                    deliveryPq.add(new Node(idx, -tmp));
+                    break;
+                }
+            }
+            
+            tmp = cap;
+            // dis까지 간 뒤 돌아오는 길에 cap만큼 상자를 수거
+            while(!pickupPq.isEmpty()) {
+                int idx = pickupPq.peek().idx;
+                tmp -= pickupPq.poll().val;
+                if(tmp < 0) {
+                    pickupPq.add(new Node(idx, -tmp));
+                    break;
+                }
+            }
+            
+            // printPq(deliveryPq, "delivery");
+            // printPq(pickupPq, "pickup");
+        }
+    }
+    
+    private void printPq(PriorityQueue<Node> pq, String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("======").append(s).append("======\n");
+        for(Node node : pq) {
+            sb.append(node.idx + ", " + node.val).append("\n");
+        }
+        System.out.println(sb);
     }
     
     public void init(int cap, int n, int[] deliveries, int[] pickups) {
         this.cap = cap;
         this.n = n;
-        this.deliveries = deliveries.clone();
-        this.pickups = pickups;
-    }
-    
-    public long solve() {
-        int curDel = getFirstHouse(deliveries);
-        int curPickUp = getFirstHouse(pickups);
-
-        long ans = 0;
-    
-        while(true) {
-            ans += Math.max(curDel+1, curPickUp+1) * 2;
-            curDel = getNextDestination(curDel, deliveries);
-            curPickUp = getNextDestination(curPickUp, pickups);
-            if(curDel == -1 && curPickUp == -1) break;
-        }
-        return ans;
-        
-    }
-    
-    public int getFirstHouse(int[] arr) {
-        int maxIdx = -1;
+        deliveryPq = new PriorityQueue<>();
+        pickupPq = new PriorityQueue<>();
         for(int i=0; i<n; i++) {
-            if(arr[i] > 0) maxIdx = i;
-        }
-        return maxIdx;
-    }
-    
-    public void printArr(int[] arr) {
-        for(int i=0; i<n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    
-    public int getNextDestination(int curDest, int[] arr) {
-        int cnt = 0;
-        int nextDest = -1;
-        for(int i=curDest; i>=0; i--) {
-            if(cnt + arr[i] <= cap) {
-                cnt += arr[i];
-                arr[i] = 0;
-            } else {
-                int rest = cap - cnt;
-                arr[i] -= rest;
-                nextDest = i;
-                break;
+            if(deliveries[i] > 0) {
+                deliveryPq.add(new Node(i+1, deliveries[i]));
+            }
+            if(pickups[i] > 0) {
+                pickupPq.add(new Node(i+1, pickups[i]));
             }
         }
-        return nextDest;
     }
 }
