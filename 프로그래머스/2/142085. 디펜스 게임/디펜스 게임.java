ac1829f8d380df12082133cbfbf1ab363f9fca4d
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-18 13:01:40 +0900

    [level 2] Title: 디펜스 게임, Time: 100.47 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,39 +2,43 @@ import java.util.*;
 
 class Solution {
     
-    int n;
-    int k;
-    int[] enemy;
+    int n, k;
+    int[] enemies;
+    int maxRound;
+    int[] dp;
+    int ans;
     
-    public int solution(int n, int k, int[] enemy) {        
+    public int solution(int n, int k, int[] enemy) {
         init(n, k, enemy);
-        return solve();
+        solve();
+        return ans;
     }
     
-    public void init(int n, int k, int[] enemy) {
+    public void solve() {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for(int i=0; i<maxRound; i++) {
+            n -= enemies[i];
+            pq.add(enemies[i]);
+            if(n < 0) {
+                if(k > 0) {
+                    n += pq.poll();
+                    k--;
+                } else {
+                    ans = i;
+                    return ;
+                }
+            }
+            ans = Math.max(ans, i+1);
+            // System.out.println("ans : " + ans);
+        }
+    }
+    
+    public void init(int n, int k, int[] enemies) {
         this.n = n;
         this.k = k;
-        this.enemy = enemy;
+        this.enemies = enemies;
+        this.maxRound = enemies.length;
+        this.dp = new int[maxRound];
     }
     
-    
-    public int solve() {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        int answer = 0;
-        for (int i = 0; i < enemy.length; i++) {
-			n -= enemy[i];
-			pq.add(enemy[i]);
-
-			if (n < 0) {
-				if (k > 0) {
-					n += pq.poll();
-					k--;
-				} else {
-					break;
-				}
-			}
-			answer++;
-		}
-		return answer;
-    }
 }
