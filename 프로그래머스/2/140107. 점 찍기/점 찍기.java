ac13f070ff8ecd5449e08a0e0b3fa1c6a9d53e1b
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-18 13:19:20 +0900

    [level 2] Title: 점 찍기, Time: 13.11 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,41 +1,28 @@
+import java.util.*;
+
 class Solution {
     
+    long ans;
     int k, d;
     
     public long solution(int k, int d) {
         init(k, d);
-        return solve();
-        
+        solve();
+        return ans;
+    }
+    
+    public void solve() {
+        for(int y=0; y<=d; y+=k) {
+            int maxX = (int) Math.sqrt(Math.pow(d, 2) - Math.pow(y, 2));
+            // System.out.println("maxX : " + maxX + ", y : " + y);
+            long cnt = maxX / k + 1;
+            // System.out.println("cnt : " + cnt);
+            ans += cnt;
+        }
     }
     
     public void init(int k, int d) {
         this.k = k;
         this.d = d;
-    }
-    
-    public long solve() {
-        long cnt = 0L;
-        long squareD = (long) d*d;
-        for(int x=0; x<=d; x+=k) {
-            long squareY = squareD - (long)x*x;
-            int maxY = (int) Math.sqrt(squareY);
-            // int maxY = (bSearch(squareY);
-            cnt += (long) (maxY/k + 1);
-            // System.out.print("Y^2 : " + squareY + ", ");
-            // System.out.println(maxY);
-        }
-        // System.out.println("cnt : " + cnt);
-        return cnt;
-    }
-    
-    public int bSearch(long tg) {
-        int st = 0;
-        int en = d+1;
-        while(st < en) {
-            int mid = (st + en) / 2;
-            if(tg < (long)mid*mid) en = mid;
-            else st = mid+1;
-        }
-        return st-1;
     }
 }
