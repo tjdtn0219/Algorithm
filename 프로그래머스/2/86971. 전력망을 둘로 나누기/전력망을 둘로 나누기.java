1afe5e61258b5f6d06a7afe75eedc5de40d47b18
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-23 15:44:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.58 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     
-    int n, m;
-    int[][] edges;
+    int n;
+    int[][] wires;
     int ans;
     
     public int solution(int n, int[][] wires) {
@@ -13,79 +13,70 @@ class Solution {
     }
     
     public void solve() {
-        for(int removed=0; removed<m; removed++) {
+        for(int i=0; i<n-1; i++) {
             List<List<Integer>> graph = new ArrayList<>();
-            int[] parents = new int[n+1];
-            for(int i=0; i<=n; i++) {
-                parents[i] = i;
+            for(int j=0; j<=n; j++) {
+                // init Graph
+                graph.add(new ArrayList<>());
             }
-            
-            // System.out.println("removed : " + edges[removed][0] + ", " + edges[removed][1]);
-            for(int i=0; i<m; i++) {
-                if(i == removed) continue;
-                int u = edges[i][0];
-                int v = edges[i][1];
-                union(parents, u, v);
+            for(int j=0; j<n-1; j++) {
+                // set Graph
+                if(i == j) continue;
+                int u = wires[j][0];
+                int v = wires[j][1];
+                graph.get(u).add(v);
+                graph.get(v).add(u);
             }
-            for(int i=1; i<=n; i++) {
-                find(parents, i);
-            }
-            // printParents(parents);
-            ans = Math.min(ans, getDiff(parents));
+            int diff = getCntDiff(graph);
+            // System.out.println("diff  " + diff);
+            ans = Math.min(ans, diff);
         }
     }
     
-    public int getDiff(int[] parents) {
+    public int getCntDiff(List<List<Integer>> graph) {
         HashMap<Integer, Integer> cntMap = new HashMap<>();
+        boolean[] vis = new boolean[n+1];
         for(int i=1; i<=n; i++) {
-            cntMap.put(parents[i], cntMap.getOrDefault(parents[i], 0) + 1);
+            if(vis[i]) continue;
+            // System.out.println("ok i : " + i);
+            int cnt = bfs(i, graph, vis);
+            cntMap.put(i, cnt);
         }
+        int diff = 0;
         boolean flag = false;
-        int res = 0;
-        if(cntMap.keySet().size() == 2) {
-            for(int key : cntMap.keySet()) {
-                if(!flag) {
-                    res += cntMap.get(key);
-                    flag = true;
-                }
-                else {
-                    res -= cntMap.get(key); 
-                } 
-                // System.out.println("key : " + key + ", cnt : " + cntMap.get(key));
+        for(int key : cntMap.keySet()) {
+            // System.out.println("key : " + key + ", cnt : " + cntMap.get(key));
+            if(!flag) {
+                diff = cntMap.get(key);
+                flag = true;
+            } else {
+                diff = Math.abs(diff - cntMap.get(key));
             }
         }
-        // System.out.println("res : " + res);
-        return Math.abs(res);
+        return diff;
+    }
+    
+    public int bfs(int st, List<List<Integer>> graph, boolean[] vis) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(st);
+        vis[st] = true;
         
-    }
-    
-    public void printParents(int[] parents) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=n; i++) {
-            sb.append(parents[i]).append(" ");
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            cnt++;
+            for(int nxt : graph.get(cur)) {
+                if(vis[nxt]) continue;
+                q.add(nxt);
+                vis[nxt] = true;
+            }
         }
-        System.out.println(sb);
+        return cnt;
     }
     
-    public int find(int[] parents, int x) {
-        if(x == parents[x]) return x;
-        else return parents[x] = find(parents, parents[x]);
-    }
-    
-    public void union(int[] parents, int u, int v) {
-        u = find(parents, u);
-        v = find(parents, v);
-        if(u < v) {
-            parents[v] = u;
-        } else {
-            parents[u] = v;
-        }
-    }
-    
-    public void init(int n, int[][] edges) {
-        this.n = n;
-        this.m = edges.length;
+    public void init(int n, int[][] wires) {
         this.ans = n;
-        this.edges = edges;
+        this.n = n;
+        this.wires = wires;
     }
 }
