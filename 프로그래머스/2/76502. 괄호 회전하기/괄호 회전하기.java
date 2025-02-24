ec92de46152f331b377633aae9f07d66ad760efb
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-02-24 10:55:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 75.87 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,68 +2,45 @@ import java.util.*;
 
 class Solution {
     
-    Deque<Character> dq;
+    String s;
+    int n;
+    int ans;
     
     public int solution(String s) {
         init(s);
-        // init("()[{}]");
-        return solve();
+        solve();
+        return ans;
     }
     
-    public void init(String s) {
-        dq = new LinkedList<>();        
-        for(int i=0; i<s.length(); i++) {
-            // System.out.println(s.charAt(i));
-            dq.addLast(s.charAt(i));
+    public void solve() {
+        for(int i=0; i<n; i++) {
+            // System.out.println("s : " + s);
+            if(isRight(s)) {
+                ans++;
+            }
+            s = s.substring(1) + s.substring(0,1);
         }
     }
     
-    public void printDq() {
-        StringBuilder sb = new StringBuilder();
-        for(Character c : dq) {
-            sb.append(c);
-        }
-        System.out.println(sb);
-    }
-    
-    public int solve() {
-        int cnt = 0;
-        // System.out.println("isRight : " + isRight());
-        
-        // System.out.println("size : " + dq.size());
-        for(int i=0; i<dq.size(); i++) {
-            // printDq();
-            if(isRight()) cnt++;
-            rotate();
-        }
-        return cnt;
-    }
-    
-    public boolean isRight() {
-        Stack<Character> stk = new Stack();
-        for(Character c : dq) {
-            if(stk.isEmpty()) {
+    public boolean isRight(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '[' || c == '(' || c == '{') {
                 stk.add(c);
-            } else{
-                if(isPair(stk.peek(), c)) {
-                    stk.pop();
-                } else {
-                    stk.add(c);
-                }
+            } else {
+                if(stk.isEmpty()) return false;
+                if(c == ']' && stk.peek() != '[') return false;
+                if(c == ')' && stk.peek() != '(') return false;
+                if(c == '}' && stk.peek() != '{') return false;
+                stk.pop();
             }
         }
         if(!stk.isEmpty()) return false;
         return true;
     }
     
-    public boolean isPair(char c1, char c2) {
-        if(c1 == '[' && c2 == ']') return true;
-        if(c1 == '{' && c2 == '}') return true;
-        if(c1 == '(' && c2 == ')') return true;
-        return false;
-    }
-    
-    public void rotate() {
-        dq.addLast(dq.removeFirst());
+    public void init(String s) {
+        this.s = s;
+        this.n = s.length();
     }
 }
