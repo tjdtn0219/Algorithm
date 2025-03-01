c3bee617252e1c21682cf8a0594a65694c847436
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-03-01 13:07:06 +0900

    [level 2] Title: 기능개발, Time: 2.71 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,51 +3,38 @@ import java.util.*;
 class Solution {
     
     int n;
-    int[] days;
-    List<Integer> ansList;
+    int[] restDays;
     
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
         init(progresses, speeds);
-        // for(int day : days) {
-        //     System.out.println(day);
-        // }
-        solve();
+        return solve();
+    }
+    
+    public int[] solve() {
+        List<Integer> ansList = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            int cnt = 1;
+            int j = i+1;
+            for(j=i+1; j<n; j++) {
+                if(restDays[i] >= restDays[j]) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            i = j-1;
+            ansList.add(cnt);
+        }
         return ansList.stream().mapToInt(Integer::intValue).toArray();
     }
     
-    public void solve() {
-        int cur = 0;
-        ansList = new ArrayList<>();
-        
+    public void init(int[] progresses, int[] speeds) {
+        this.n = progresses.length;
+        restDays = new int[n];
         for(int i=0; i<n; i++) {
-            int num = days[i];
-            int cnt = 1;
-            for(int j=i+1; j<n; j++) {
-                if(days[j] > num) {
-                    break;
-                }
-                cnt++;
-            }
-            ansList.add(cnt);
-            i += cnt-1;
-        }
-        // for(int ans : ansList) {
-        //     System.out.println("ans : " + ans);
-        // }
-        
-    }
-    
-    public void init(int[] progress, int[] speeds) {
-        this.n = progress.length;
-        days = new int[n];
-        for(int i=0; i<n; i++) {
-            days[i] = (100 - progress[i]) / speeds[i];
-            if((100 - progress[i]) % speeds[i] > 0) {
-                days[i]++;
-            }
+            int day = (100 - progresses[i]) / speeds[i];
+            if((100 - progresses[i]) % speeds[i] != 0) day++;
+            restDays[i] = day;
         }
     }
-    
-    
 }
