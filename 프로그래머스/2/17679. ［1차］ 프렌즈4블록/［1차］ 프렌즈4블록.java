fc8f7669fba51540a64988748bd6aacb532da750
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-03-06 10:49:03 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 21.51 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -2,123 +2,109 @@ import java.util.*;
 
 class Solution {
     
-    static final int[] DX = {1, 0, 1};
-    static final int[] DY = {0, 1, 1};
-    static final char X = '-';
+    static final int[] DX = {1,0,1};
+    static final int[] DY = {0,1,1};
+    
     int n, m;
     char[][] board;
-    boolean hasCheck;
+    int ans;
     
     public int solution(int m, int n, String[] board) {
         init(m, n, board);
-        return solve();
+        solve();
+        return ans;
     }
     
-    public int solve() {
-        int cnt = 0;
-        // System.out.println("==INIT==");
-        // printBoard();
-        while(true) {
-            hasCheck = false;
-            boolean[][] check = getCheck();
-            if(!hasCheck) break;
-            delete(check);
-            // System.out.println("==DELETE==");
-            // printBoard();
-            reLocate();
-            // System.out.println("==RELOCATE==");
-            // printBoard();
-            // cnt++;
-            // if(cnt == 5) break;
-        }
-        // printBoard();
-        return getXCnt();
-    }
-    
-    public int getXCnt() {
-        int cnt = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(board[i][j] == X) {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-    
-    public void printBoard() {
+    private void printBoard() {
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                sb.append(board[i][j]);
+                sb.append(board[i][j]).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
     }
     
-    public void reLocate() {
+    public void solve() {
+        while(true) {
+            // printBoard();
+            boolean[][] isDelete = new boolean[n][m];
+            boolean flag = checkDelete(isDelete);
+            if(!flag) break;
+            
+            deleteBlocks(isDelete);
+            fallBlocks();
+        }
+        
+    }
+    
+    public void fallBlocks() {
         for(int i=n-2; i>=0; i--) {
             for(int j=0; j<m; j++) {
-                if(board[i][j] == X) continue;
-                int ti = i;
-                while((ti+1 <= n-1) && (board[ti+1][j] == X)) {
-                    ti++;
-                }
-                // System.out.println("i, j : " + i + ", " + j + " | ti : " + ti);
-                if(ti > i) {
-                    board[ti][j] = board[i][j];
-                    board[i][j] = X;   
-                }
+                fallBlock(i, j);
             }
+        }   
+    }
+    
+    public void fallBlock(int x, int y) {
+        int nx = x + 1;
+        boolean flag = false;
+        while(nx < n && board[nx][y] == '.') {
+            flag = true;
+            nx++;
+        }
+        if(flag) {
+            --nx;
+            board[nx][y] = board[x][y];
+            board[x][y] = '.';
         }
     }
     
-    public void delete(boolean[][] check) {
+    public void deleteBlocks(boolean[][] isDelete) {
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(check[i][j]) {
-                    board[i][j] = X;
+                if(isDelete[i][j]) {
+                    board[i][j] = '.';
+                    ans++;
                 }
             }
         }
     }
     
-    public boolean[][] getCheck() {
-        boolean[][] check = new boolean[n][m];
+    public boolean checkDelete(boolean[][] isDelete) {
+        boolean flag = false;
+        
         for(int i=0; i<n-1; i++) {
             for(int j=0; j<m-1; j++) {
-                if(board[i][j] == X) continue;
-                boolean flag = true;
-                for(int dir=0; dir<3; dir++) {
-                    int nx = i + DX[dir];
-                    int ny = j + DY[dir];
-                    if(board[nx][ny] != board[i][j]) {
-                        flag = false;
-                    }
-                }
-                if(flag) {
-                    hasCheck = true;
-                    check[i][j] = true;
-                    for(int dir=0; dir<3; dir++) {
-                        int nx = i + DX[dir];
-                        int ny = j + DY[dir];
-                        check[nx][ny] = true;
+                if(board[i][j] == '.') continue;
+                if(isSquare(i, j)) {
+                    flag = true;
+                    isDelete[i][j] = true;
+                    for(int d=0; d<3; d++) {
+                        isDelete[i+DX[d]][j+DY[d]] = true;
                     }
                 }
             }
         }
-        return check;
+        return flag;
+    }
+    
+    public boolean isSquare(int x, int y) {
+        for(int i=0; i<3; i++) {
+            int nx = x + DX[i];
+            int ny = y + DY[i];
+            if(board[nx][ny] != board[x][y]) return false;
+        }
+        return true;
     }
     
     public void init(int m, int n, String[] board) {
         this.n = m;
         this.m = n;
-        System.out.println(this.n + " " + this.m);
         this.board = new char[this.n][this.m];
-        for(int i=0; i<this.n; i++) {
-            for(int j=0; j<this.m; j++) {
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
                 this.board[i][j] = board[i].charAt(j);
             }
         }
