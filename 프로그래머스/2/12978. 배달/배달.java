7adf52fa7f843f0cf12070508ef755e1361253a6
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-03-06 11:12:49 +0900

    [level 2] Title: 배달, Time: 7.78 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-class Node {
+class Edge {
     int b;
     int c;
-    public Node(int b, int c) {
+    public Edge(int b, int c) {
         this.b = b;
         this.c = c;
     }
@@ -11,45 +11,48 @@ class Node {
 
 class Solution {
     
-    int n;
-    int k;
-    List<List<Node>> graph;
+    int n, k;
+    List<List<Edge>> graph;
     int[] dis;
     int ans;
     
     public int solution(int N, int[][] road, int K) {
-        init(N, road, K);
+
+        init(N, K, road);
         solve();
+
         return ans;
     }
     
     public void solve() {
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
-            return o1.c - o2.c;
-        });
-        pq.add(new Node(1, 0));
-        dis[1] = 0;
-        
-        while(!pq.isEmpty()) {
-            Node cur = pq.poll();
-            if(cur.c > dis[cur.b]) continue;
-            for(Node nxt : graph.get(cur.b)) {
-                if(dis[cur.b] + nxt.c < dis[nxt.b]) {
-                    dis[nxt.b] = dis[cur.b] + nxt.c;
-                    pq.add(new Node(nxt.b, dis[nxt.b]));
-                }
-            }
-        }
+        dikjstra(1);
         for(int i=1; i<=n; i++) {
             if(dis[i] <= k) ans++;
         }
     }
     
-    public void init(int n, int[][] road, int k) {
+    public void dikjstra(int start) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.c - o2.c);
+        dis[start] = 0;
+        pq.add(new Edge(start, 0));
+        
+        while(!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            if(dis[cur.b] < cur.c) continue;
+            // System.out.println("cur : " + cur.b);
+            for(Edge nxt : graph.get(cur.b)) {
+                // System.out.println("nxt : " + nxt.b + ", cost : " + nxt.c);
+                if(dis[cur.b] + nxt.c < dis[nxt.b]) {
+                    dis[nxt.b] = dis[cur.b] + nxt.c;
+                    pq.add(new Edge(nxt.b, dis[nxt.b]));
+                }
+            }
+        }
+    }
+    
+    public void init(int n, int k, int[][] road) {
         this.n = n;
         this.k = k;
-        this.dis = new int[n+1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
         graph = new ArrayList<>();
         for(int i=0; i<=n; i++) {
             graph.add(new ArrayList<>());
@@ -58,8 +61,10 @@ class Solution {
             int a = edge[0];
             int b = edge[1];
             int c = edge[2];
-            graph.get(a).add(new Node(b, c));
-            graph.get(b).add(new Node(a, c));
+            graph.get(a).add(new Edge(b, c));
+            graph.get(b).add(new Edge(a, c));
         }
+        dis = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
     }
 }
