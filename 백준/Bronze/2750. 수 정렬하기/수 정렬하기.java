d8b88aedc8f51722c5b5b672ecffa05e8acfa1f2
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-03-16 10:22:51 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
@@ -11,7 +10,7 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+n= Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
 
@@ -19,7 +18,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        sort(arr);
+sort(arr);
 
         for(int i=0; i<n; i++) System.out.println(arr[i]);
         System.out.println();
@@ -27,7 +26,57 @@ public class Main {
     }
 
     public static void sort(int[] arr) {
-        quickSort(arr, 0, n-1);
+//        quickSort(arr, 0, n-1);
+        bubbleSort(arr);
+//        mergeSort(arr, 0,n-1);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if(low >= high) return;
+
+        int mid = (low + high)/2;
+
+mergeSort(arr, low, mid);
+mergeSort(arr, mid+1, high);
+
+merge(arr, low, mid, high);
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high-low+1];
+        int idx = 0;
+
+        int left = low;
+        int right = mid+1;
+        while(left<=mid && right<=high) {
+            if(arr[left] <= arr[right]) {
+                temp[idx++] = arr[left++];
+            } else {
+                temp[idx++] = arr[right++];
+            }
+        }
+        while(left<=mid) {
+            temp[idx++] = arr[left++];
+        }
+        while(right<=high) {
+            temp[idx++] = arr[right++];
+        }
+        for(int i=low; i<=high; i++) {
+            arr[i] = temp[i-low];
+        }
     }
 
     public static void quickSort(int[] arr, int low, int high) {
