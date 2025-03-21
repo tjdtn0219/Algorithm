673bb5d5d2779cd02d20c21c80abb85b8b2f8b36
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-03-21 20:04:43 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 25740 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,38 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
+    public static List<List<Integer>> tree = new ArrayList<>();
+    public static int[] parent;
+    public static int[] depth;
 
     public static void main(String[] args) throws IOException {
 
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    	String[] strings = br.readLine().split(" ");
-    	int n = Integer.parseInt(strings[0]);
-    	int s = Integer.parseInt(strings[1]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    	int[] arr = new int[n+1];
-    	strings = br.readLine().split(" ");
-    	for(int i=0; i<n; i++) arr[i] = Integer.parseInt(strings[i]);
-    	
-    	int l = 0;
-    	int r = 0;
-    	int sum = arr[0];
-    	int len = n+1;
-    	
-    	while(l<=r && r<n) {
-    		if(sum < s) {
-    			sum += arr[++r];
-    		} else {
-    			len = Math.min(len, r-l+1);
-    			sum -= arr[l++];
-    		}
-    		
-    	}
-    	if(len == n+1) len = 0;
-    	System.out.println(len);
+        String[] strings = br.readLine().split(" ");
+        int n = Integer.parseInt(strings[0]);
+        int s = Integer.parseInt(strings[1]);
+
+        strings = br.readLine().split(" ");
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        for(int i=0; i<n; i++) {
+            arr[i+1] = Integer.parseInt(strings[i]);
+            dp[i+1] = arr[i+1];
+        }
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = arr[i] + dp[i-1];
+        }
+
+        int left = 1;
+        int right = 1;
+
+        int ans = Integer.MAX_VALUE;
+        while(left <= right && right<=n) {
+            int partSum = dp[right] - dp[left-1];
+            if(partSum < s) right++;
+            else {
+                ans = Math.min(ans, right-left+1);
+                left++;
+            }
+        }
+
+        if(ans == Integer.MAX_VALUE) ans = 0;
+
+        System.out.println(ans);
+
     }
 }
