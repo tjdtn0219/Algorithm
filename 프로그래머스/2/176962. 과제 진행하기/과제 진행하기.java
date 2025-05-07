d27abcde2d7ca3e85336d8f7937d89fdf0464735
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-05-07 14:54:44 +0900

    [level 2] Title: 과제 진행하기, Time: 7.95 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -2,83 +2,99 @@ import java.util.*;
 
 class Task {
     String name;
-    int startTime;
-    int playTime;
-    
-    public Task(String name, int startTime, int playTime) {
+    int start;
+    int rest;
+    public Task(String name, int start, int rest) {
         this.name = name;
-        this.startTime = startTime;
-        this.playTime = playTime;
+        this.start = start;
+        this.rest = rest;
     }
 }
 
 class Solution {
     
-    List<Task> taskList;
-    Stack<Task> stk;
+    int n;
+    PriorityQueue<Task> pq;
+    Deque<Task> stoppedQ;
+    List<String> ansList;
     
     public String[] solution(String[][] plans) {
-        
         init(plans);
-        return solve();
-    }
-    
-    public String[] solve() {
-        List<String> ansList = new ArrayList<>();
-        Collections.sort(taskList, (t1, t2) -> t1.startTime - t2.startTime);
-        Task cur = taskList.get(0);
-        
-        for(int i=1; i<taskList.size(); i++) {
-            Task nxt = taskList.get(i);
-            // System.out.println("cur : " + cur.name + ", nxt : " + nxt.name);
-            if(cur.startTime + cur.playTime < nxt.startTime) {
-                // System.out.println("TAG1");
-                ansList.add(cur.name);
-                int restTime = nxt.startTime - (cur.startTime + cur.playTime);
-                
-                while(!stk.isEmpty()) {
-                    Task task = stk.pop();
-                    if(task.playTime <= restTime) {
-                        restTime -= task.playTime;
-                        ansList.add(task.name);
-                    } else {
-                        task.playTime -= restTime;
-                        stk.push(task);
-                        break;
-                    }
-                }
-            } else if(cur.startTime + cur.playTime > nxt.startTime) {
-                // System.out.println("TAG2");
-                cur.playTime = cur.startTime + cur.playTime - nxt.startTime;
-                stk.push(cur);
-            } else {
-                // System.out.println("TAG3");
-                ansList.add(cur.name);
-            }
-            cur = nxt;
-        }
-        
-        ansList.add(cur.name);
-
-        while(!stk.isEmpty()) {
-            ansList.add(stk.pop().name);
-        }
-
+        solve();
         return ansList.toArray(new String[0]);
     }
     
-    public void init(String[][] plans) {
-        taskList = new ArrayList<>();
-        stk = new Stack<>();
-        for(String[] plan : plans) {
-            taskList.add(new Task(plan[0], timeToAmount(plan[1]), Integer.parseInt(plan[2])));
+    public void solve() {
+        Task cur = pq.poll();
+        int curT = cur.start;
+        
+        while(!pq.isEmpty()) {
+            Task nxt = pq.poll();
+            // System.out.println("cur : " + cur.name + ", nxt : " + nxt.name + ", curT : " + curT);
+            if(curT + cur.rest <= nxt.start) {
+                // System.out.println("TAG1 : " + cur.name);
+                //현재 끝나자마자 다음꺼 바로 시작
+                ansList.add(cur.name);
+                
+                int availT = nxt.start - (curT + cur.rest);
+                while(!stoppedQ.isEmpty()) {
+                    //현재 끝났는데 시간 여유 있을 때
+                    Task task = stoppedQ.pollLast();
+                    if(task.rest <= availT) {
+                        ansList.add(task.name);
+                        availT -= task.rest;
+                    } else {
+                        task.rest -= availT;
+                        stoppedQ.addLast(task);
+                        break;
+                    }
+                }
+                cur = nxt;
+                curT = nxt.start;
+            } else {
+                // System.out.println("TAG-stop : " + cur.name);
+                //중간 멈춤
+                cur.rest = curT + cur.rest - nxt.start;
+                stoppedQ.addLast(cur);
+                cur = nxt;
+                curT = nxt.start;
+            }
+            if(pq.isEmpty()) {
+                // System.out.println("last : " + nxt.name);
+                ansList.add(nxt.name);
+            }
+            // printAns();
+        }
+        
+        System.out.println(stoppedQ.size());
+        
+        while(!stoppedQ.isEmpty()) {
+            ansList.add(stoppedQ.pollLast().name);
         }
     }
     
-    public int timeToAmount(String time) {
-        String[] tmp = time.split(":");
-        int h = Integer.parseInt(tmp[0]);
-        int m = Integer.parseInt(tmp[1]);
-        return 60*h + m;
+    private void printAns() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ans : ");
+        for(String s : ansList) {
+            sb.append(s).append(" ");
+        }
+        System.out.println(sb);
+    }
+    
+    public void init(String[][] plans) {
+        this.pq = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
+        this.stoppedQ = new LinkedList<>();
+        for(String[] plan : plans) {
+            pq.add(new Task(plan[0], toInt(plan[1]), Integer.parseInt(plan[2])));
+        }
+        ansList = new ArrayList<>();
+    }
+    
+    public int toInt(String time) {
+        String[] splits = time.split(":");
+        int h = Integer.parseInt(splits[0]);
+        int m = Integer.parseInt(splits[1]);
+        return h*60 + m;
     }
 }
