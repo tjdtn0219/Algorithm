39ac4aafbfe3bf47867f13c2a92e293a36777f26
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-10 19:02:02 +0900

    [level 2] Title: 순위 검색, Time: 1058.25 ms, Memory: 372 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,84 +1,109 @@
+import java.awt.Point;
 import java.util.*;
 
 class Solution {
     
-    static final String[] LANG = {"java", "python", "cpp"};
+    static final String[] LANG = {"cpp", "java", "python"};
     static final String[] POS = {"backend", "frontend"};
     static final String[] EXP = {"junior", "senior"};
     static final String[] FOOD = {"chicken", "pizza"};
     
     HashMap<String, List<Integer>> scoreListMap;
+    String[] infos;
+    String[] queries;
     int[] answer;
-    List<String> keyList;
     
     public int[] solution(String[] info, String[] query) {
-        answer = new int[query.length];
-        initScoreListMap(info);
-        doQuery(query);
+        init(info, query);
+        solve();
         return answer;
     }
     
-
-    public void doQuery(String[] queries) {
+    public void solve() {
         int idx = 0;
-        for(String query : queries) {
-            String[] tmp = query.split(" and ");
-            int score = Integer.parseInt(tmp[3].split(" ")[1]);
-            tmp[3] = tmp[3].split(" ")[0];
-            // System.out.println("query : " + query);
-            keyList = new ArrayList<>();
-            dfs(tmp, 0, "");
-            int cnt = 0;
-            for(String key : keyList) {
-                cnt += getCnt(key, score);
+        for(String q : queries) {
+            List<Pair> pairList = getPairs(q);
+            int sum = 0;
+            for(Pair pair : pairList) {
+                sum += doQuery(pair);
+                // System.out.println("sum : " + sum);
             }
-            answer[idx++] = cnt;
+            // System.out.println("---------");
+            answer[idx++] = sum;
         }
     }
     
-    public int getCnt(String key, int target) {
+    public int doQuery(Pair pair) {
+        String key = pair.key;
+        int score = pair.score;
+        // System.out.println("key : " + key + ", score : " + score);
         List<Integer> scoreList = scoreListMap.get(key);
+        // System.out.println("scoreList : " + scoreList);
+        return getOverCnt(scoreList, score);
+    }
+    
+    public int getOverCnt(List<Integer> list, int tg) {
         int st = 0;
-        int en = scoreList.size();
+        int en = list.size();
         
         while(st < en) {
             int mid = (st + en) / 2;
-            if(target <= scoreList.get(mid)) en = mid;
+            if(tg <= list.get(mid)) en = mid;
             else st = mid + 1;
         }
-        return scoreList.size() - st;
+
+        return list.size() - st;
     }
     
-    public void dfs(String[] tmp, int k, String s) {
-        if(k == 4) {
-            keyList.add(s);
-            // System.out.println("key : " + s);
-            return ;
-        }
-        
-        if(k==0 && tmp[k].equals("-")) {
-            for(String lang : LANG) {
-                dfs(tmp, k+1, s + lang);
-            }
-        } else if(k==1 && tmp[k].equals("-")) {
-            for(String pos : POS) {
-                dfs(tmp, k+1, s + pos);
-            }
-        } else if(k==2 && tmp[k].equals("-")) {
-            for(String exp : EXP) {
-                dfs(tmp, k+1, s + exp);
-            }
-        } else if(k==3 && tmp[k].equals("-")) {
-            for(String food : FOOD) {
-                dfs(tmp, k+1, s + food);
-            }
+    public List<Pair> getPairs(String q) {
+        String[] tmp = q.split(" ");
+        String lang = tmp[0];
+        String pos = tmp[2];
+        String exp = tmp[4];
+        String food = tmp[6];
+        int score = Integer.parseInt(tmp[7]);
+        List<String> langList = new ArrayList<>();
+        List<String> posList = new ArrayList<>();
+        List<String> expList = new ArrayList<>();
+        List<String> foodList = new ArrayList<>();
+        if(lang.equals("-")) {
+            Collections.addAll(langList, LANG);
         } else {
-            dfs(tmp, k+1, s + tmp[k]);
+            langList.add(lang);
+        }
+        if(pos.equals("-")) {
+            Collections.addAll(posList, POS);
+        } else {
+            posList.add(pos);
+        }
+        if(exp.equals("-")) {
+            Collections.addAll(expList, EXP);
+        } else {
+            expList.add(exp);
+        }
+        if(food.equals("-")) {
+            Collections.addAll(foodList, FOOD);
+        } else {
+            foodList.add(food);
         }
         
+        List<Pair> pairList = new ArrayList<>();
+        for(String l : langList) {
+            for(String p : posList) {
+                for(String e : expList) {
+                    for(String f : foodList) {
+                        String key = l + p + e + f;
+                        pairList.add(new Pair(key, score));
+                    }
+                }
+            }
+        }
+        return pairList;
     }
     
-    public void initScoreListMap(String[] infos) {
+    public void init(String[] infos, String[] queries) {
+        this.queries = queries;
+        this.answer = new int[queries.length];
         scoreListMap = new HashMap<>();
         for(String lang : LANG) {
             for(String pos : POS) {
@@ -90,24 +115,35 @@ class Solution {
                 }
             }
         }
-        setScoreListMap(infos);
-    }
-    
-    public void setScoreListMap(String[] infos) {
+        
         for(String info : infos) {
-            String[] tmp = info.split(" ");
-            String lang = tmp[0];
-            String pos = tmp[1];
-            String exp = tmp[2];
-            String food = tmp[3];
-            int score = Integer.parseInt(tmp[4]);
-            String key = lang + pos + exp + food;
-            List<Integer> list = scoreListMap.get(key);
-            list.add(score);
+            Pair pair = parseInfo(info);
+            // System.out.println("key : " + pair.key + " , score : " + pair.score);
+            scoreListMap.get(pair.key).add(pair.score);
         }
+        
         for(String key : scoreListMap.keySet()) {
             Collections.sort(scoreListMap.get(key));
         }
+        
     }
     
+    public Pair parseInfo(String info) {
+        String[] tmp = info.split(" ");
+        String lang = tmp[0];
+        String pos = tmp[1];
+        String exp = tmp[2];
+        String food = tmp[3];
+        int score = Integer.parseInt(tmp[4]);
+        return new Pair(lang + pos + exp + food, score);
+    }
+}
+
+class Pair {
+    String key;
+    Integer score;
+    public Pair(String key, Integer score) {
+        this.key = key;
+        this.score = score;
+    }
 }
