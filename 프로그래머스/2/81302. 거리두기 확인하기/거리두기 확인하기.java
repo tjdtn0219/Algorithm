b0bd773402b1ffa758de1e66cbc59a913e54d3a3
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-10 18:04:54 +0900

    [level 2] Title: 거리두기 확인하기, Time: 46.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,60 +1,49 @@
+import java.awt.Point;
 import java.util.*;
 
 class Solution {
     
-    static final int[] DX = {0,1,0,-1}; //동 남 서 북
-    static final int[] DY = {1,0,-1,0};
     static final int N = 5;
+    static final int[] DX = {1,0,-1,0};
+    static final int[] DY = {0,-1,0,1};
     
-    int[][] board;
-    boolean[][] vis;
-    List<Point> people;
+    char[][][] board;
+    int[] answer;
     
     public int[] solution(String[][] places) {
-        int[] answer = new int[N];
-        for(int i=0; i<places.length; i++) {
-            answer[i] = solve(places[i]);
-        }
+        init(places);
+        solve();
         return answer;
     }
     
-    public int solve(String[] place) {
-        init(place);
-        return isKeepDistance();
+    public void solve() {
+        for(int k=0; k<N; k++) {
+            check(k);
+        }
     }
     
-    public void init(String[] place) {
-        board = new int[N][N];
-        people = new ArrayList<>();
+    public void check(int k) {
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                if(place[i].charAt(j) == 'P') {
-                    board[i][j] = 1;
-                    people.add(new Point(i, j));
-                }
-                else if(place[i].charAt(j) == 'X') {
-                    board[i][j] = -1;
+                if(board[k][i][j] == 'P') {
+                    if(!bfs(k, i, j)) {
+                        answer[k] = 0;
+                        return ;
+                    }
                 }
             }
         }
     }
     
-    public int isKeepDistance() {
-        for(Point person : people) {
-            if(isExistSurround(person)) {
-                return 0;
-            }
-        }
-        return 1;
-    }
-    
-    public boolean isExistSurround(Point person) {
-        vis = new boolean[N][N];
+    public boolean bfs(int k, int x, int y) {
+        boolean[][] vis = new boolean[N][N];
+        
         Queue<Point> q = new LinkedList<>();
-        q.add(person);
-        vis[person.x][person.y] = true;
-        int depth = 0;
-        while(!q.isEmpty() && depth<2) {
+        q.add(new Point(x, y));
+        vis[x][y] = true;
+        int cnt = 0;
+        
+        while(!q.isEmpty() && cnt < 2) {
             int size = q.size();
             for(int i=0; i<size; i++) {
                 Point cur = q.poll();
@@ -63,28 +52,32 @@ class Solution {
                     int ny = cur.y + DY[dir];
                     if(!isInner(nx, ny)) continue;
                     if(vis[nx][ny]) continue;
-                    if(board[nx][ny] == -1) continue;
-                    if(board[nx][ny] == 1) return true;
+                    if(board[k][nx][ny] == 'X') continue;
+                    if(board[k][nx][ny] == 'P') return false;
                     q.add(new Point(nx, ny));
                     vis[nx][ny] = true;
                 }
+                cnt++;
             }
-            depth++;
         }
-        return false;
+
+        return true;
     }
     
     public boolean isInner(int x, int y) {
         return 0<=x && 0<=y && x<N && y<N;
     }
     
-}
-
-class Point {
-    int x;
-    int y;
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public void init(String[][] places) {
+        board = new char[N][N][N];
+        for(int k=0; k<N; k++) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    board[k][i][j] = places[k][i].charAt(j);
+                }
+            }
+        }
+        answer = new int[N];
+        Arrays.fill(answer, 1);
     }
 }
