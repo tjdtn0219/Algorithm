0ffc1022e26cfedf11fde8603ca2570e3b7528d0
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-12 22:48:45 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4163.51 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -2,92 +2,134 @@ import java.util.*;
 
 class Solution {
     
-    List<HashSet<Character>> orderList;
-    int[] course;
+    List<String> answer;
     List<Character> menuList;
+    HashMap<Integer, HashSet<Character>> orderMap;
+    int[] courses;
     char[] comb;
+    HashMap<Integer, Pair> pairMap;
+    int maxCnt;
     HashMap<Integer, List<String>> cntMap;
-    int max;
-    List<String> ansList;
     
     public String[] solution(String[] orders, int[] course) {
         init(orders, course);
         solve();
-        Collections.sort(ansList);
-        return ansList.toArray(new String[0]);
+        Collections.sort(answer);
+        return answer.toArray(new String[0]);
     }
     
     public void solve() {
-        for(int cnt : course) {
+        for(int cnt : courses) {
             comb = new char[cnt];
-            max = 0;
+            maxCnt = 0;
             cntMap = new HashMap<>();
-            dfs(0, cnt, 0);
-            if(max == 0) continue;
-            for(String s : cntMap.get(max)) {
-                ansList.add(s);
+            makeComb(0, cnt, 0);
+            // System.out.println("maxCnt : " + maxCnt);
+            if(maxCnt == 0) {
+                continue;
             }
-        }   
+            for(String s : cntMap.get(maxCnt)) {
+                answer.add(s);
+            }
+        }
+        // printAnswer();
+        
     }
     
-    public void dfs(int k, int cnt, int last) {
-        if(k == cnt) {
-            int orderCnt = getOrderCnt(comb);
-            if(2 <= orderCnt && max <= orderCnt) {
-                max = orderCnt;
-                List<String> list = cntMap.getOrDefault(orderCnt, new ArrayList<>());
-                String s = arrToStr(comb);
-                list.add(s);
-                cntMap.put(orderCnt, list);
+    public void printAnswer() {
+        System.out.println("ANSWER : ");
+        for(String s : answer) {
+            System.out.println("s : " + s);
+        }
+    }
+    
+    public void makeComb(int k, int limit, int startIdx) {
+        if(k == limit) {
+            // printComb();
+            Pair pair = getPair(comb);
+            if(pair == null) {
+                return ;
+            }
+            if(pair.cnt >= maxCnt) {
+                List<String> list = cntMap.getOrDefault(pair.cnt, new ArrayList<>());
+                list.add(pair.menu);
+                cntMap.put(pair.cnt, list);
+                maxCnt = pair.cnt;
             }
             return ;
         }
         
-        for(int i=last; i<menuList.size(); i++) {
+        for(int i=startIdx; i<menuList.size(); i++) {
             comb[k] = menuList.get(i);
-            dfs(k+1, cnt, i+1);
+            makeComb(k+1, limit, i+1);
         }
+        
+    }
+    
+    public Pair getPair(char[] comb) {
+        int cnt = 0;
+        for(int key : orderMap.keySet()) {
+            if(isOrder(orderMap.get(key), comb)) {
+                cnt++;
+            }
+        }
+        if(cnt >= 2) {
+            return new Pair(arrToStr(comb), cnt);
+        } else {
+            return null;
+        }
+    }
+    
+    public boolean isOrder(HashSet<Character> orderSet, char[] comb) {
+        for(char c : comb) {
+            if(!orderSet.contains(c)) {
+                return false;
+            }
+        }
+        return true;
     }
     
     public String arrToStr(char[] arr) {
         StringBuilder sb = new StringBuilder();
-        Arrays.sort(arr);
         for(char c : arr) {
             sb.append(c);
         }
         return sb.toString();
     }
     
-    public int getOrderCnt(char[] comb) {
-        int cnt = 0;
-        for(HashSet<Character> set : orderList) {
-            boolean flag = true;
-            for(char c : comb) {
-                if(!set.contains(c)) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) cnt++;
+    private void printComb() {
+        StringBuilder sb = new StringBuilder();
+        for(char c : comb) {
+            sb.append(c + " ");
         }
-        return cnt;
+        System.out.println(sb);
     }
     
-    public void init(String[] orders, int[] course) {
-        orderList = new ArrayList<>();
-        this.course = course;
+    public void init(String[] orders, int[] courses) {
+        this.answer = new ArrayList<>();
+        this.orderMap = new HashMap<>();
+        this.courses = courses;
         HashSet<Character> menuSet = new HashSet<>();
+        int idx = 0;
         for(String order : orders) {
-            HashSet<Character> orderSet = new HashSet<>();
+            HashSet<Character> tmpSet = new HashSet<>();
             for(char c : order.toCharArray()) {
                 menuSet.add(c);
-                orderSet.add(c);
+                tmpSet.add(c);
             }
-            orderList.add(orderSet);
+            orderMap.put(idx++, tmpSet);
         }
-        menuList = new ArrayList<>(menuSet);
-        Collections.sort(menuList);
-        ansList = new ArrayList<>();
+        this.menuList = new ArrayList<>(menuSet);
+        
+    }
+}
 
+class Pair {
+    String menu;
+    int cnt;
+    
+    public Pair(String menu, int cnt) {
+        this.menu = menu;
+        this.cnt = cnt;
     }
 }
