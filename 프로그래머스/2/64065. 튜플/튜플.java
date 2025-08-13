7305f5373c7b3a7c0d555dbce936bc1bc301410f
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-13 22:30:08 +0900

    [level 2] Title: 튜플, Time: 108.81 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,42 +2,53 @@ import java.util.*;
 
 class Solution {
     
-    static final int MAX = 100_000;
+    HashMap<Integer, HashSet<Integer>> tupleMap;
+    int[] answer;
     
     public int[] solution(String s) {
-        s.substring(2, s.length() - 2);
-        String[] splits = s.split("\\{");
-        HashMap<Integer, HashSet<Integer>> setMap = new HashMap<>();
-        
-        int max = 0;
-        List<Integer> ansList = new ArrayList<>();
-        for(String str : splits) {
-            if(str.length() == 0) continue;
-            HashSet<Integer> set = getSet(str.substring(0, str.length()-2));
-            setMap.put(set.size(), set);
-            max++;
-        }
-        
-        boolean[] vis = new boolean[MAX+1];
-        for(int i=1; i<=max; i++) {
-            HashSet<Integer> set = setMap.get(i);
-            for(int num : set) {
-                if(vis[num]) continue;
-                ansList.add(num);
-                vis[num] = true;
-            }
-        }
-        
-        return ansList.stream().mapToInt(Integer::intValue).toArray();
+        init(s);
+        solve();
+        return answer;
     }
     
-    public HashSet<Integer> getSet(String s) {
-        // System.out.println("s : " + s);
+    public void solve() {
         HashSet<Integer> set = new HashSet<>();
+        for(int i=1; i<=tupleMap.size(); i++) {
+            for(int prev : tupleMap.get(i)) {
+                if(!set.contains(prev)) {
+                    answer[i-1] = prev;
+                    set.add(prev);
+                }
+            }
+        }
+    }
+    
+    public void init(String s) {
+        this.tupleMap = new HashMap<>();
+        String[] tmp = parseStr(s.substring(1, s.length()-2));
+        for(String str : tmp) {
+            HashSet<Integer> set = toSet(str);
+            tupleMap.put(set.size(), set);
+        }
+        this.answer = new int[tmp.length];
+    }
+    
+    public HashSet<Integer> toSet(String s) {
         String[] splits = s.split(",");
-        for(String num : splits) {
-            set.add(Integer.parseInt(num));
+        HashSet<Integer> set = new HashSet<>();
+        for(String str : splits) {
+            set.add(Integer.parseInt(str));
         }
         return set;
+    }
+    
+    public String[] parseStr(String s) {
+        String[] splits = s.split("},");
+        String[] result = new String[splits.length];
+        for(int i=0; i<splits.length; i++) {
+            result[i] = splits[i].substring(1);
+            // System.out.println(result[i]);
+        }   
+        return result;
     }
 }
