49246263134eeb1cf89075cf7fb3eb2c13302565
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-15 21:31:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 135.07 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,49 +2,75 @@ import java.util.*;
 
 class Solution {
     
-    Queue<Integer> q1;
-    Queue<Integer> q2;
+    int answer;
+    Queue<Long> q1;
+    Queue<Long> q2;
     long sum1;
-    long sum2;    
+    long sum2;
     
     public int solution(int[] queue1, int[] queue2) {
-        int answer = -2;
-        init(queue1, queue2);
-        return solve();
-    }
-    
-    public int solve() {
-        int len = q1.size() * 4;
         
-        for(int i=0; i<len; i++) {
-            if(sum1 == sum2) {
-                return i;
-            }
-            if(sum1 > sum2) {
-                int polled = q1.poll();
-                q2.add(polled);
-                sum1 -= polled;
-                sum2 += polled;
-            } else {
-                int polled = q2.poll();
-                q1.add(polled);
-                sum1 += polled;
-                sum2 -= polled;
-            }
-        }
-        return -1;
+        init(queue1, queue2);
+        solve();
+        return answer;
     }
     
-    public void init(int[] queue1, int[] queue2) {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
-        for(int n : queue1) {
-            q1.add(n);
-            sum1 += n;
+    public void solve() {
+        // 3 2 7 2 -> 14
+        // 4 6 5 1 -> 16
+        
+        // 3 2 7 2 4 -> 18
+        // 6 5 1 -> 12
+        
+        // 2 7 2 4 -> 15
+        // 6 5 1 3 -> 15
+        if((sum1 + sum2) % 2 == 1) {
+            answer = -1;
+            return ;
         }
-        for(int n : queue2) {
-            q2.add(n);
-            sum2 += n;
+        System.out.println(q1.peek());
+        long tg = (sum1 + sum2) / 2;
+        int n = q1.size();
+        for(int i=0; i<4*n; i++) {
+            // System.out.println("sum1 : " + sum1 + ", sum2 : " + sum2);
+            // printQ(q1, 1);
+            // printQ(q2, 2);
+            if(sum1 < sum2) {
+                sum1 += q2.peek();
+                sum2 -= q2.peek();
+                q1.add(q2.poll());
+            } else if(sum1 > sum2) {
+                sum2 += q1.peek();
+                sum1 -= q1.peek();
+                q2.add(q1.poll());
+            } else {
+                answer = i;
+                return ;
+            }
+        }
+        answer = -1;
+    }
+    
+    public void printQ(Queue<Long> q, int idx) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("q").append(idx).append(" : ");
+        for(long n : q) {
+            sb.append(n + " ");
+        }
+        System.out.println(sb);
+    }
+    
+    public void init(int[] q1, int[] q2) {
+        this.q1 = new LinkedList<>();
+        this.q2 = new LinkedList<>();
+        for(int num : q1) {
+            this.q1.add((long) num);
+            sum1 += (long) num;
+        }
+        
+        for(int num : q2) {
+            this.q2.add((long) num);
+            sum2 += (long) num;
         }
     }
 }
