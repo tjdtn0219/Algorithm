6fc4bb1dcd27760e7f273b54512bf42610cbb581
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-17 23:23:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 41.31 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,71 +2,72 @@ import java.util.*;
 
 class Solution {
     
-    int n;
     int len;
     int maxW;
-    int[] truckWeights;
-    int ans;
-    Queue<Integer> crossQ;
+    Queue<Integer> readyQ;
+    Queue<Integer> q;
+    int n;
+    int answer;
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         init(bridge_length, weight, truck_weights);
         solve();
-        return ans;
+        return answer;
     }
     
-    private void printCrossQ() {
+    public void solve() {
+        int sumW = 0;
+        int t = 0;
+        while(!readyQ.isEmpty()) {
+            // printQ(q);
+            // printQ(readyQ);
+            // System.out.println("t : " + t + " ----------------");
+            int nxt = readyQ.peek();
+            sumW -= q.poll();
+            if(sumW + nxt <= maxW) {
+                q.add(nxt);
+                readyQ.poll();
+                sumW += nxt;
+            } else {
+                q.add(0);
+            }
+            t++;
+        }
+        int idx = 0;
+        int maxIdx = 0;
+        while(!q.isEmpty()) {
+            if(q.poll() > 0) {
+                maxIdx = idx + 1;
+            }
+            idx++;
+        }
+        t += maxIdx;
+        answer = t;
+    }
+    
+    public void printQ(Queue<Integer> q) {
         StringBuilder sb = new StringBuilder();
-        for(int num : crossQ) {
-            sb.append(num).append(" ");
+        sb.append("q : ");
+        for(int n : q) {
+            sb.append(n).append(" ");
         }
         System.out.println(sb);
     }
     
-    public void solve() {
-        int idx = 0;
-        int successCnt = 0;
-        int sumW = 0;
-        int t = 0;
-        while(idx < n) {
-            int nxt = truckWeights[idx];
-            // printCrossQ();
-            // System.out.println("nxt : " + nxt);
-            int polled = crossQ.poll();
-            if(polled > 0) {
-                successCnt++;
-                sumW -= polled;
-            }
-            if(sumW + nxt <= maxW) {
-                crossQ.add(nxt);
-                sumW += nxt;
-                idx++;
-            } else {
-                crossQ.add(0);
-            }
-            t++;
-            
+    public void init(int len, int maxW, int[] weights) {
+        this.n = weights.length;
+        this.len = len;
+        this.maxW = maxW;
+        this.readyQ = new LinkedList<>();
+        for(int w : weights) {
+            readyQ.add(w);
         }
-        System.out.println("T1 : " + t);
-        
-        while(!crossQ.isEmpty() && successCnt < n) {
-            if(crossQ.poll() > 0) {
-                successCnt++;
-            }
-            t++;
-        }
-        System.out.println("T2 : " + t);
-        ans = t;
-    }
-    
-    public void init(int bridge_length, int weight, int[] truck_weights) {
-        this.len = bridge_length;
-        this.maxW = weight;
-        this.truckWeights = truck_weights;
-        this.n = truck_weights.length;
-        crossQ = new LinkedList<>();
+        this.q = new LinkedList<>(); 
         for(int i=0; i<len; i++) {
-            crossQ.add(0);
+            q.add(0);
         }
+        // printQ(readyQ);
+        // printQ(q);
+        // System.out.println("--------");
     }
 }
