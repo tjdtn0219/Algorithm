d8b616aacbce188f4880cd006a19767302a18712
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-19 18:39:02 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 44.71 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,19 +1,44 @@
-import java.util.*;
 
-class Solution {
-    
-    int[][] board;
-    int[][] dp;
+
+class Solution
+{
     int n, m;
-    int ans = 0;
+    int[][] board;
+    int answer;
     
-    public int solution(int [][]board) {
+    public int solution(int [][]board)
+    {
         init(board);
         solve();
-        return ans;
+        return answer;
+    }
+    
+    public void solve() {
+        int[][] dp = new int[n][m];
+        for(int i=0; i<n; i++) {
+            dp[i] = board[i].clone();
+        }
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                if(board[i][j] == 0) continue;
+                if(dp[i-1][j] > 0 && dp[i][j-1] > 0 && dp[i-1][j-1] > 0) {
+                    int min = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    dp[i][j] = min + 1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+        answer = answer * answer;
+        // printArr(dp);
     }
     
     private void printArr(int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
@@ -21,41 +46,14 @@ class Solution {
             }
             sb.append("\n");
         }
+        sb.append("=====================");
         System.out.println(sb);
     }
     
-    public void solve() {
-        // printArr(board);
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(board[i][j] == 0) continue;
-                int min = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
-                dp[i][j] = min + 1;
-            }
-        }
-        // printArr(dp);
-        int maxLen = getMaxLen();
-        ans = maxLen * maxLen;
-    }
-    
-    public int getMaxLen() {
-        int maxLen = -1;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                maxLen = Math.max(maxLen, dp[i][j]);
-            }
-        }
-        return maxLen;
-    }
-    
     public void init(int[][] board) {
-        this.board = board;
         this.n = board.length;
         this.m = board[0].length;
-        this.dp = new int[n][m];
-        for(int i=0; i<n; i++) {
-            dp[i] = board[i].clone();
-        }
-        System.out.println("n : " + n + ", m : " + m);
+        this.board = board;
     }
+    
 }
