538873b23a0cc7b28507b19a14082c6a92276a16
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-19 18:12:31 +0900

    [level 2] Title: 양궁대회, Time: 11.22 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 class Solution {
     
-    static final int TARGET_CNT = 11;
+    static final int N = 11;
     
     int n;
-    int[] apeachInfo;
-    int[] scoreLimit;
-    int[] lionInfo;
-    int maxDiff;
+    int[] apeach;
+    int[] lion;
     int[] answer;
+    int maxDiff;
     HashMap<Integer, List<int[]>> answerMap;
     
     public int[] solution(int n, int[] info) {
@@ -19,80 +18,71 @@ class Solution {
         return answer;
     }
     
-    public void init(int n, int[] info) {
-        this.n = n;
-        apeachInfo = info;
-        scoreLimit = new int[TARGET_CNT];
-        for(int i=0; i<TARGET_CNT; i++) {
-            scoreLimit[i] = apeachInfo[i] + 1;
-        }
-        lionInfo = new int[TARGET_CNT];
-        maxDiff = 0;
-        answer = new int[TARGET_CNT];
-        answerMap = new HashMap<>();
-    }
-    
     public void solve() {
-        dfs(0, 0);
-        List<int[]> list = answerMap.getOrDefault(maxDiff, new ArrayList<>());
-        if(list.size() == 0) {
+        makeComb(0, 0);
+        if(maxDiff == 0) {
             answer = new int[1];
             answer[0] = -1;
-        } else {
-            Collections.sort(list, (o1, o2) -> {
-               for(int i=TARGET_CNT-1; i>=0; i--) {
-                   if(o1[i] != o2[i]) return o2[i] - o1[i];
-               }
-                return 0;
-            });
-            answer = list.get(0);
+            return ;
         }
-        
-        System.out.println("maxDiff : " + maxDiff);
+        List<int[]> list = answerMap.get(maxDiff);
+        Collections.sort(list, (arr1, arr2) -> {
+            for(int i=N-1; i>=0; i--) {
+                if(arr1[i] != arr2[i]) return arr2[i] - arr1[i];
+            }
+            return 0;
+        });
+        answer = list.get(0);
     }
     
-    public void dfs(int k, int start) {
-        if(k==n) {
-            int[] scores = getScore();
-            int apeachScore = scores[0];
-            int lionScore = scores[1];
-            if(apeachScore < lionScore) {
-                int diff = lionScore - apeachScore;
-                if(diff >= maxDiff) {
-                    List<int[]> list = answerMap.getOrDefault(diff, new ArrayList<>());
-                    list.add(lionInfo.clone());
-                    answerMap.put(diff, list);
-                    maxDiff = diff;
-                }
+    public void makeComb(int k, int last) {
+        if(k == n) {
+            int scoreDiff = getScoreDiff();
+            if(scoreDiff > 0 && scoreDiff >= maxDiff) {
+                List<int[]> list = answerMap.getOrDefault(scoreDiff, new ArrayList<>());
+                list.add(lion.clone());
+                answerMap.put(scoreDiff, list);
+                maxDiff = scoreDiff;
             }
             return ;
         }
         
-        for(int i=start; i<TARGET_CNT; i++) {
-            if(apeachInfo[i] < lionInfo[i]) continue;
-                lionInfo[i]++;
-                dfs(k+1, i);
-                lionInfo[i]--;
+        for(int i=last; i<N; i++) {
+            lion[i]++;
+            makeComb(k+1, i);
+            lion[i]--;
         }
-        
     }
     
-    public int[] getScore() {
+    public int getScoreDiff() {
         int apeachScore = 0;
         int lionScore = 0;
-        for(int i=0; i<TARGET_CNT; i++) {
-            if(apeachInfo[i] == 0 && lionInfo[i] == 0) continue;
-            if(apeachInfo[i] < lionInfo[i]) {
-                lionScore += 10 - i;
-            } else {
+        for(int i=0; i<N; i++) {
+            if(apeach[i] >= lion[i] && apeach[i] > 0) {
                 apeachScore += 10 - i;
+            } else if(apeach[i] < lion[i]) {
+                lionScore += 10 - i;
             }
         }
-        int[] scores = new int[2];
-        scores[0] = apeachScore;
-        scores[1] = lionScore;
-        return scores;
+        // System.out.println("lionScore : " + lionScore);
+        // System.out.println("apeachScore : " + apeachScore);
+        return lionScore - apeachScore;
     }
     
- 
+    private void printLion() {
+        StringBuilder sb = new StringBuilder();
+        for(int n : lion) {
+            sb.append(n).append(" ");
+        }
+        System.out.println(sb);
+    }
+    
+    public void init(int n, int[] infos) {
+        this.n = n;
+        this.apeach = infos;
+        this.lion = new int[N];
+        this.answer = new int[N];
+        this.maxDiff = 0;
+        this.answerMap = new HashMap<>();
+    }
 }
