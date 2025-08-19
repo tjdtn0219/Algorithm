673a1b8e273ed0642b9cba2319b5a63400262550
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-19 19:05:41 +0900

    [level 2] Title: 단체사진 찍기, Time: 1430.07 ms, Memory: 413 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,43 +1,48 @@
 import java.util.*;
 
-class Operation {
-    char a;
-    char b;
+class Data {
+    char c1;
+    char c2;
     char op;
-    int num;
-    
-    public Operation(char a, char b, char op, int num) {
-        this.a = a;
-        this.b = b;
+    int val;
+    public Data(char c1, char c2, char op, int val) {
+        this.c1 = c1;
+        this.c2 = c2;
         this.op = op;
-        this.num = num;
+        this.val = val;
     }
 }
 
 class Solution {
     
-    public static final char[] FRIENDS = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    public static final int N = 8;
+    static final int N = 8;
+    static final char[] FRIENDS = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
     
+    int answer;
     int n;
-    List<Operation> opList;
+    List<Data> dataList;
     char[] comb;
-    int ans;
+    boolean[] vis;
     
     public int solution(int n, String[] data) {
+        
         init(n, data);
+        for(Data d : dataList) {
+            System.out.println(d.c1 + " " + d.c2 + " " + d.op + " " + d.val);
+        }
         solve();
-        return ans;
+        return answer;
     }
     
     public void solve() {
-        makeComb(0, new boolean[N]);
+        makeComb(0);
     }
     
-    public void makeComb(int k, boolean[] vis) {
+    public void makeComb(int k) {
         if(k == N) {
+            // printComb();
             if(isOkay()) {
-                ans++;
+                answer++;
             }
             return ;
         }
@@ -46,44 +51,57 @@ class Solution {
             if(vis[i]) continue;
             vis[i] = true;
             comb[k] = FRIENDS[i];
-            makeComb(k+1, vis);
+            makeComb(k+1);
             vis[i] = false;
         }
     }
     
     public boolean isOkay() {
-        HashMap<Character, Integer> posMap = new HashMap<>();
-        for(int i=0; i<N; i++) {
-            posMap.put(comb[i], i);
-        }
-        for(Operation operation : opList) {
-            char a = operation.a;
-            char b = operation.b;
-            char op = operation.op;
-            int num = operation.num;
-            
-            int dis = Math.abs(posMap.get(a) - posMap.get(b)) - 1;
-            if(op == '<' && dis >= num) {
-                return false;
-            } else if(op == '>' && dis <= num) {
-                return false;
-            } else if(op == '=' && dis != num) {
-                return false;
+        HashMap<Character, Integer> map = toMap(comb);
+        for(Data data : dataList) {
+            int dis = Math.abs(map.get(data.c1) - map.get(data.c2)) - 1;
+            if(data.op == '=') {
+                if(dis != data.val) return false;
+            } else if(data.op == '>') {
+                if(dis <= data.val) return false;
+            } else if(data.op == '<') {
+                if(dis >= data.val) return false;
             }
         }
         return true;
     }
     
-    public void init(int n, String[] datas) {
-        this.opList = new ArrayList<>();
-        this.n = n;
-        this.comb = new char[N];
-        for(String data : datas) {
-            char a = data.charAt(0);
-            char b = data.charAt(2);
-            char op = data.charAt(3);
-            int num = data.charAt(4) - '0';
-            opList.add(new Operation(a, b, op, num));
+    public HashMap<Character, Integer> toMap(char[] arr) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<N; i++) {
+            map.put(arr[i], i);
         }
+        return map;
+    }
+    
+    private void printComb() {
+        StringBuilder sb = new StringBuilder();
+        for(char c : comb) {
+            sb.append(c).append(" ");
+        }
+        System.out.println(sb);
+    }
+    
+    public void init(int n, String[] datas) {
+        this.n = n;
+        this.dataList = new ArrayList<>();
+        for(String data : datas) {
+            dataList.add(parseStr(data));
+        }
+        this.comb = new char[N];
+        this.vis = new boolean[N];
+    }
+    
+    public Data parseStr(String str) {
+        char c1 = str.charAt(0);
+        char c2 = str.charAt(2);
+        char op = str.charAt(3);
+        int val = str.charAt(4) - '0';
+        return new Data(c1, c2, op, val);
     }
 }
