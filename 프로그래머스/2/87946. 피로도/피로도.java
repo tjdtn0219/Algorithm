a9d7aead7e4c6e046cc9fb2aebf38260c7b49b28
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-08-21 23:21:59 +0900

    [level 2] Title: 피로도, Time: 5.56 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,42 +2,68 @@ import java.util.*;
 
 class Solution {
     
-    int k;
-    int n;
+    int answer;
+    int cur;
     int[][] dungeons;
+    int n;
+    int[] comb;
     boolean[] vis;
-    int ans;
     
     public int solution(int k, int[][] dungeons) {
+
         init(k, dungeons);
         solve();
-        return ans;
+        return answer;
     }
     
     public void solve() {
-        dfs(k, 0);
+        makeComb(0);
     }
     
-    public void dfs(int curK, int dep) {
-        ans = Math.max(ans, dep);
-        if(dep == n) {
+    public void makeComb(int k) {
+        if(k == n) {
+            // printComb();
+            play();
             return ;
         }
         
         for(int i=0; i<n; i++) {
             if(vis[i]) continue;
-            if(dungeons[i][0] <= curK) {
-                vis[i] = true;
-                dfs(curK - dungeons[i][1], dep+1);
-                vis[i] = false;
-            }
+            vis[i] = true;
+            comb[k] = i;
+            makeComb(k+1);
+            vis[i] = false;
         }
     }
     
+    public void play() {
+        int cur = this.cur;
+        int cnt = 0;
+        for(int idx : comb) {
+            // System.out.println("dungeon : " + dungeon[0] + ", cur : " + cur + ", cnt : " + cnt);
+            if(cur >= dungeons[idx][0]) {
+                // System.out.println("enter");
+                cnt++;
+                cur -= dungeons[idx][1];
+            }
+        }
+        // System.out.println("cnt : " + cnt);
+        answer = Math.max(answer, cnt);   
+    }
+    
+    private void printComb() {
+        StringBuilder sb = new StringBuilder();
+        for(int num : comb) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+    
     public void init(int k, int[][] dungeons) {
-        this.k = k;
-        this.n = dungeons.length;
+        this.cur = k;
         this.dungeons = dungeons;
+        this.n = dungeons.length;
+        this.comb = new int[n];
         this.vis = new boolean[n];
     }
 }
