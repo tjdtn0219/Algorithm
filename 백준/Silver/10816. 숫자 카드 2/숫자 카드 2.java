88e812ac0fd8e26aa27fef3b76570cd90972b2fe
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-10-14 18:23:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1404 ms, Memory: 143888 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,83 +1,82 @@
-import java.io.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static final int MAX = Integer.MAX_VALUE;
+    static int n, m;
+    static int[] arr;
+    static int[] cards;
 
-    int n;
-    int[] arr1;
-    int m;
-    int[] arr2;
-
-    public static void main(String[] args) {
-        new Main().solution();
-    }
-
-    public void solution() {
+    public static void main(String[] args) throws Exception {
         input();
         solve();
     }
 
-    public void input() {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            n = Integer.parseInt(br.readLine());
-            arr1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            Arrays.sort(arr1);
-            // printArr(arr1);
-            m = Integer.parseInt(br.readLine());
-            arr2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        } catch (Exception e) {
-            System.out.println("INPUT ERROR!!");
-            throw new RuntimeException(e);
+    public static void solve() {
+        Arrays.sort(arr);
+        int[] ans = new int[m];
+        for(int i=0; i<m; i++) {
+            int card = cards[i];
+            ans[i] = getUpperIdx(card) - getLowerIdx(card);
         }
-    }
-
-    public void solve() {
         StringBuilder sb = new StringBuilder();
-        for(int num : arr2) {
-            sb.append(getUpperIdx(num) - getLowerIdx(num)).append(" ");
-        }
-        System.out.println(sb);
-    }
-
-    public int getLowerIdx(int tg) {
-        int st = 0;
-        int en = n;
-
-        while(st < en) {
-            int mid = (st + en) / 2;
-            if(tg <= arr1[mid]) {
-                en = mid;
-            } else {
-                st = mid + 1;
-            }
-        }
-        return st;
-    }
-
-    public int getUpperIdx(int tg) {
-        int st = 0;
-        int en = n;
-
-        while(st < en) {
-            int mid = (st + en) / 2;
-            if(tg < arr1[mid]) {
-                en = mid;
-            } else {
-                st = mid + 1;
-            }
-        }
-        return st;
-    }
-
-    private void printArr(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for(int num : arr) {
+        for(int num : ans) {
             sb.append(num).append(" ");
         }
         System.out.println(sb);
     }
 
+    public static int getLowerIdx(int tg) {
+        int st = 0;
+        int en = n;
+
+        while(st < en) {
+            int mid = (st + en) / 2;
+            if(tg <= arr[mid]) {
+                en = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+
+        return st;
+    }
+
+    public static int getUpperIdx(int tg) {
+        int st = 0;
+        int en = n;
+
+        while(st < en) {
+            int mid = (st + en) / 2;
+            if(tg < arr[mid]) {
+                en = mid;
+            } else {
+                st = mid + 1;
+            }
+        }
+
+        return st;
+    }
+
+    public static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            arr = new int[n];
+            String[] tmp = br.readLine().split(" ");
+            for(int i=0; i<n; i++) {
+                arr[i] = Integer.parseInt(tmp[i]);
+            }
+            m = Integer.parseInt(br.readLine());
+            cards = new int[m];
+            tmp = br.readLine().split(" ");
+            for(int i=0; i<m; i++) {
+                cards[i] = Integer.parseInt(tmp[i]);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("INPUT ERROR");
+        }
+    }
 }
