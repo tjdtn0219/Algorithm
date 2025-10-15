88b97385b910fc1502bae047f9e4056926799ed4
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-10-15 14:05:32 +0900

    [level 3] Title: 등대, Time: 66.92 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -2,53 +2,57 @@ import java.util.*;
 
 class Solution {
     
+    int answer;
     int n;
     List<List<Integer>> graph;
-    int ans;
     int[][] dp;
-    // dp[n][0] : n번째 등대가 불이 꺼져있는 경우 최소 불 켜진 등대 개수
-    // dp[n][1] : n번째 등대가 불이 켜져있는 경우 최소 불 켜진 등대 개수
+    boolean[] vis;
+    //dp[i][0]: i번째 등대가 꺼져있는 경우 불 켜진 최소 등대 수
+    //dp[i][1]: i번째 등대가 켜져있는 경우 불 켜진 최소 등대 수
     
     public int solution(int n, int[][] lighthouse) {
         init(n, lighthouse);
         solve();
-        return ans;
+        return answer;
     }
     
     public void solve() {
-        dfs(1, new boolean[n+1]);
-        ans = Math.min(dp[1][0], dp[1][1]);
+        int[] res = dfs(1);
+        answer = Math.min(res[0], res[1]);
     }
     
-    public void dfs(int cur, boolean[] vis) {
+    public int[] dfs(int cur) {
         vis[cur] = true;
-        dp[cur][0] = 0; //cur 번째 등대가 불이 꺼진 경우
-        dp[cur][1] = 1; //cur 번째 등대가 불이 켜진 경우
+        dp[cur][0] = 0;
+        dp[cur][1] = 1;
         
         int max = Integer.MAX_VALUE;
         for(int nxt : graph.get(cur)) {
             if(vis[nxt]) continue;
-            dfs(nxt, vis);
-            dp[cur][0] += dp[nxt][1];
-            dp[cur][1] += Math.min(dp[nxt][0], dp[nxt][1]);
+            int[] tmp = dfs(nxt);
+            dp[cur][0] += tmp[1];
+            dp[cur][1] += Math.min(tmp[0], tmp[1]);
         }
+        
+        return dp[cur];
     }
     
-    public void init(int n, int[][] lighthouse) {
+    public void init(int n, int[][] edges) {
         this.n = n;
-        graph = new ArrayList<>();
+        this.dp = new int[n+1][2];
+        for(int i=0; i<=n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        this.vis = new boolean[n+1];
+        this.graph = new ArrayList<>();
         for(int i=0; i<=n; i++) {
             graph.add(new ArrayList<>());
         }
-        for(int[] edge : lighthouse) {
+        for(int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             graph.get(u).add(v);
             graph.get(v).add(u);
-        }
-        this.dp = new int[n+1][2];
-        for(int i=0; i<=n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
     }
 }
