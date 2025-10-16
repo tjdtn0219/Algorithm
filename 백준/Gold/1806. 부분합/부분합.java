fc9c61499c35ae187793fa981fa7712da5837964
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-10-16 14:10:36 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 25292 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,48 +1,56 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static List<List<Integer>> tree = new ArrayList<>();
-    public static int[] parent;
-    public static int[] depth;
+    static int n, s;
+    static int[] arr;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void solve() {
+        int left = 0;
+        int right = 0;
+        int sum = arr[0];
+        int len = Integer.MAX_VALUE;
 
-        String[] strings = br.readLine().split(" ");
-        int n = Integer.parseInt(strings[0]);
-        int s = Integer.parseInt(strings[1]);
-
-        strings = br.readLine().split(" ");
-        int[] arr = new int[n+1];
-        int[] dp = new int[n+1];
-        for(int i=0; i<n; i++) {
-            arr[i+1] = Integer.parseInt(strings[i]);
-            dp[i+1] = arr[i+1];
-        }
-
-        for(int i=2; i<=n; i++) {
-            dp[i] = arr[i] + dp[i-1];
-        }
-
-        int left = 1;
-        int right = 1;
-
-        int ans = Integer.MAX_VALUE;
-        while(left <= right && right<=n) {
-            int partSum = dp[right] - dp[left-1];
-            if(partSum < s) right++;
-            else {
-                ans = Math.min(ans, right-left+1);
-                left++;
+        while(left <= right && right < n) {
+            // System.out.println("left : " + left + " , right : " + right + ", sum : " + sum + ", len : " + len);
+            if(sum >= s) {
+                len = Math.min(len, right - left + 1);
+                sum -= arr[left++];
+            } else {
+                if(right < n-1) {
+                    sum += arr[++right];
+                } else {
+                    right++;
+                }
             }
         }
 
-        if(ans == Integer.MAX_VALUE) ans = 0;
+        int answer = len == Integer.MAX_VALUE ? 0 : len;
 
-        System.out.println(ans);
+        System.out.println(answer);
+    }
 
+
+    public static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] tmp = br.readLine().split(" ");
+            n = Integer.parseInt(tmp[0]);
+            s = Integer.parseInt(tmp[1]);
+            arr = new int[n+1];
+            tmp = br.readLine().split(" ");
+            for(int i=0; i<n; i++) {
+                arr[i] = Integer.parseInt(tmp[i]);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("INPUT ERROR");
+        }
     }
 }
