53e6bf4946fd79b6d7f42363fa3fecfb35e565df
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-10-17 16:34:55 +0900

    [Gold II] Title: 문제집, Time: 524 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,22 +1,50 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    int n, m;
-    List<List<Integer>> graph;
-    int[] inDegree;
+    static int n, m;
+    static int[] inDegree;
+    static List<List<Integer>> graph;
 
-    public static void main(String[] args) {
-        new Main().solution();
-    }
-
-    public void solution() {
+    public static void main(String[] args) throws Exception {
         input();
         solve();
     }
 
-    public void input() {
+    public static void solve() {
+        boolean[] vis = new boolean[n+1];
+        Queue<Integer> pq = new PriorityQueue<>((o1, o2) ->o1 - o2);
+        for(int i=1; i<=n; i++) {
+            if(inDegree[i] == 0) {
+                pq.add(i);
+            }
+        }
+
+
+        List<Integer> ansList = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            int cur = pq.poll();
+            if(vis[cur]) continue;
+            vis[cur] = true;
+            ansList.add(cur);
+            for(int nxt : graph.get(cur)) {
+                inDegree[nxt]--;
+                if(inDegree[nxt] == 0) {
+                    pq.add(nxt);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int num : ansList) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    public static void input() {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String[] tmp = br.readLine().split(" ");
@@ -29,46 +57,13 @@ public class Main {
             }
             for(int i=0; i<m; i++) {
                 tmp = br.readLine().split(" ");
-                int u = Integer.parseInt(tmp[0]);
-                int v = Integer.parseInt(tmp[1]);
-                graph.get(u).add(v);
-                inDegree[v]++;
+                int a = Integer.parseInt(tmp[0]);
+                int b = Integer.parseInt(tmp[1]);
+                graph.get(a).add(b);
+                inDegree[b]++;
             }
-        } catch (Exception e) {
-            System.out.println("INPUT ERROR!!!");
-            throw new RuntimeException(e);
+        } catch(Exception e) {
+            throw new RuntimeException("INPUT ERROR");
         }
-    }
-
-    public void solve() {
-        List<Integer> list = new ArrayList<>();
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=1; i<=n; i++) {
-            if(inDegree[i] == 0) {
-                pq.add(i);
-            }
-        }
-
-        while(!pq.isEmpty()) {
-            int cur = pq.poll();
-            list.add(cur);
-            for(int nxt : graph.get(cur)) {
-                inDegree[nxt]--;
-                if(inDegree[nxt] == 0) {
-                    pq.add(nxt);
-                }
-            }
-        }
-
-        printAnswer(list);
-    }
-
-    private static void printAnswer(List<Integer> answerList) {
-        StringBuilder sb = new StringBuilder();
-        for(int num : answerList) {
-            sb.append(num + " ");
-        }
-        System.out.println(sb);
     }
 }
