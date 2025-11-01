a3bc163ade416f2a8a82ee778e557ef32a0039c3
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-11-01 15:32:32 +0900

    [Gold I] Title: 구간 합 구하기, Time: 588 ms, Memory: 120524 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,38 +1,96 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    int N, M, K;
-    long[] arr;
-    List<String> inputList;
-    long[] tree;
-
-    public static void main(String[] args) {
-        new Main().solution();
+    static class Node {
+        int a, b;
+        long c;
+        public Node(int a, int b, long c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
     }
 
-    public void solution() {
+    static int n, m, k;
+    static long[] arr;
+    static List<Node> nodeList;
+    static long[] tree;
+
+    public static void main(String[] args) throws Exception {
         input();
-        initTree();
         solve();
     }
 
+    public static void solve() {
+        initTree();
+        StringBuilder sb = new StringBuilder();
+        for(Node node : nodeList) {
+            int a = node.a;
+            int b = node.b;
+            long c = node.c;
+            if(a == 1) {
+                long diff = c - arr[b];
+                arr[b] = c;
+                update(1, n, 1, b, diff);
+            } else {
+                long sum = getSum(1, n, 1, b, (int) c);
+                sb.append(sum).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 
-    public void input() {
+    public static void update(int st, int en, int node, int idx, long diff) {
+        if(idx < st || idx > en) return ;
+        tree[node] += diff;
+        if(st == en) return ;
+        int mid = (st + en) / 2;
+        update(st, mid, node*2, idx, diff);
+        update(mid + 1, en, node*2 + 1, idx, diff);
+    }
+
+    public static long getSum(int st, int en, int node, int left, int right) {
+        if(left > en || right < st) return 0;
+        if(left <= st && en <= right) return tree[node];
+        int mid = (st + en) / 2;
+        return getSum(st, mid, node*2, left, right)
+                + getSum(mid+1, en, node*2 + 1, left, right);
+    }
+
+    public static long initTreeDfs(int st, int en, int node) {
+        if(st == en) return tree[node] = arr[st];
+        int mid = (st + en) / 2;
+        return tree[node] = initTreeDfs(st, mid, node * 2) + initTreeDfs(mid+1, en, node*2 + 1);
+    }
+
+    public static void initTree() {
+        int h = (int) Math.ceil(Math.log(n) / Math.log(2)) + 1;
+        int size = (int) Math.pow(2, h) - 1;
+        tree = new long[n*4];
+        initTreeDfs(1, n, 1);
+    }
+
+    public static void input() {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String[] N_M_K = br.readLine().split(" ");
-            N = Integer.parseInt(N_M_K[0]);
-            M = Integer.parseInt(N_M_K[1]);
-            K = Integer.parseInt(N_M_K[2]);
-            arr = new long[N+1];
-            for(int i=0; i<N; i++) {
+            String[] n_m_k = br.readLine().split(" ");
+            n = Integer.parseInt(n_m_k[0]);
+            m = Integer.parseInt(n_m_k[1]);
+            k = Integer.parseInt(n_m_k[2]);
+            arr = new long[n+1];
+            for(int i=0; i<n; i++) {
                 arr[i+1] = Long.parseLong(br.readLine());
             }
-            inputList = new ArrayList<>();
-            for(int i=0; i<M+K; i++) {
-                inputList.add(br.readLine());
+            nodeList = new ArrayList<>();
+            for(int i=0; i<m+k; i++) {
+                String[] a_b_c = br.readLine().split(" ");
+                int a = Integer.parseInt(a_b_c[0]);
+                int b = Integer.parseInt(a_b_c[1]);
+                long c = Long.parseLong(a_b_c[2]);
+                nodeList.add(new Node(a, b, c));
             }
 
         } catch (Exception e) {
@@ -41,52 +99,4 @@ public class Main {
         }
     }
 
-    public void initTree() {
-        tree = new long[4*N];
-        initTreeDfs(1, N, 1);
-    }
-
-    public long initTreeDfs(int st, int en, int node) {
-        if(st == en) return tree[node] = arr[st];
-        int mid = (st + en) / 2;
-        return tree[node] = initTreeDfs(st, mid, node * 2) + initTreeDfs(mid+1, en, node*2 + 1);
-    }
-
-    public void update(int st, int en, int node, int idx, long diff) {
-        if(idx < st || idx > en) return ;
-        tree[node] += diff;
-        if(st == en) return ;
-        int mid = (st + en) / 2;
-        update(st, mid, node*2, idx, diff);
-        update(mid+1, en, node*2 + 1, idx, diff);
-    }
-
-    public long sum(int st, int en, int node, int left, int right) {
-        if(left > en || right < st) return 0;
-        if(left <= st && en <= right) return tree[node];
-        int mid = (st + en) / 2;
-        return sum(st, mid, node*2, left, right) + sum(mid+1, en, node*2 + 1, left, right);
-    }
-
-    public void solve() {
-        List<Long> answerList = new ArrayList<>();
-        for(String input : inputList) {
-            String[] tmp = input.split(" ");
-            int a = Integer.parseInt(tmp[0]);
-            int b = Integer.parseInt(tmp[1]);
-            long c = Long.parseLong(tmp[2]);
-            if(a == 1) {
-                long diff = c - arr[b];
-                arr[b] = c;
-                update(1, N, 1, b, diff);
-            } else {
-                answerList.add(sum(1, N, 1, b, (int) c));
-            }
-        }
-        for(long answer : answerList) {
-            System.out.println(answer);
-        }
-    }
-
 }
-
