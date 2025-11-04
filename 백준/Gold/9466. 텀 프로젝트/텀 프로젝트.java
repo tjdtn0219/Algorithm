aa10d9cdf9222be077209c58689971678496fd41
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-11-04 14:35:59 +0900

    [Gold III] Title: 텀 프로젝트, Time: 952 ms, Memory: 302204 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,86 +1,94 @@
+import java.awt.Point;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static int[] arr;
-    public static int n;
-    public static int[] state;
+    static final int VIS = 1;
+    static final int NOT_CYCLE = 2;
+    static final int CYCLE = 3;
 
-    public static final int NOT_VISITED = 0;
-    public static final int VISITED = 1;
-    public static final int CYCLE_IN = 2;
-    public static final int NOT_CYCLE_IN = 3;
+    static int T, N;
+    static int[] arr;
+    static int[] state;
 
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(bf.readLine());
-        int[] ans = new int[T];
-
-        for (int t = 0; t < T; t++) {
-            int n = Integer.parseInt(bf.readLine());
-            arr = new int[n+1];
-            state = new int[n+1];
-            String[] inputs = bf.readLine().split(" ");
-            for(int i = 1; i <= n; i++) {
-                arr[i] = Integer.parseInt(inputs[i - 1]);
-            }
-
-            for(int i=1; i<=n; i++) {
-                if(state[i] == NOT_VISITED) run(i);
-            }
-            int cnt = 0;
-            for(int i=1; i<=n; i++) {
-                if(state[i] == NOT_CYCLE_IN) cnt++;
-            }
-            System.out.println(cnt);
-        }
+    public static void main(String[] args) throws Exception {
+        input();
+//        solve();
     }
 
-    public static void run(int x) {
+    public static void solve() {
+        state = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            if(state[i] == 0) {
+                bfs(i);
+            }
+        }
+
+        int ans = 0;
+        for(int i=1; i<=N; i++) {
+            if(state[i] == NOT_CYCLE) ans++;
+        }
+        System.out.println(ans);
+    }
+
+    public static void bfs(int x) {
         int cur = x;
+
         while(true) {
-            state[cur] = VISITED;
+            state[cur] = VIS;
             cur = arr[cur];
 
-            // 1번 케이스
-            if(state[cur] == CYCLE_IN || state[cur] == NOT_CYCLE_IN) {
+            if(state[cur] == CYCLE || state[cur] == NOT_CYCLE) {
                 cur = x;
-                while(state[cur] == VISITED) {
-                    state[cur] = NOT_CYCLE_IN;
+                while(state[cur] == VIS) {
+                    state[cur] = NOT_CYCLE;
                     cur = arr[cur];
                 }
-                return;
+                return ;
             }
 
-            // 2번 케이스
-            if(state[cur] == VISITED && cur != x) {
-                while(state[cur]==VISITED) {
-                    state[cur] = CYCLE_IN;
+            if(state[cur] == VIS && cur != x) {
+                while(state[cur] == VIS) {
+                    state[cur] = CYCLE;
                     cur = arr[cur];
                 }
                 cur = x;
-                while(state[cur] != CYCLE_IN) {
-                    state[cur] = NOT_CYCLE_IN;
+                while(state[cur] == VIS) {
+                    state[cur] = NOT_CYCLE;
                     cur = arr[cur];
                 }
-                return;
+                return ;
             }
-
-            // 3번 케이스
-            if(state[cur] == VISITED && cur == x) {
-                while(state[cur]==VISITED) {
-                    state[cur] = CYCLE_IN;
+            if(state[cur] == VIS && cur==x) {
+                while(state[cur] == VIS) {
+                    state[cur] = CYCLE;
                     cur = arr[cur];
                 }
+
                 return;
             }
-
         }
     }
+
+    public static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            T = Integer.parseInt(br.readLine());
+            for(int t=0; t<T; t++) {
+                N = Integer.parseInt(br.readLine());
+                arr = new int[N+1];
+                String[] tmp = br.readLine().split(" ");
+                for(int i=0; i<N; i++) {
+                    arr[i+1] = Integer.parseInt(tmp[i]);
+                }
+                solve();
+            }
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!!!!");
+            throw new RuntimeException(e);
+        }
+    }
+
 }
