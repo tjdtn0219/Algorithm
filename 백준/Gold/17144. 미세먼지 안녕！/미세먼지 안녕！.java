3e782e8b70e04aaf1ca96379cd42318354186e61
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-11-05 20:36:10 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 304 ms, Memory: 29744 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,131 +1,174 @@
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class Main {
 
-    public static final int[] dx = {1,0,-1,0};
-    public static final int[] dy = {0,-1,0,1};
-    public static int[][] map;
-    public static int n,m,t;
+    static class Cleaner {
+        Point p1, p2;
+        public Cleaner(Point p1, Point p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+
+    static final int[] DX = {1,0,-1,0};
+    static final int[] DY = {0,-1,0,1};
+
+    static int n, m;
+    static int T;
+    static int[][] board;
+    static Cleaner cleaner;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input();
+        solve();
+    }
 
-        String[] strings = br.readLine().split(" ");
-
-        n = Integer.parseInt(strings[0]);
-        m = Integer.parseInt(strings[1]);
-        t = Integer.parseInt(strings[2]);
-
-        int acRow = 0;
-
-        map = new int[n][m];
-        for(int i=0; i<n; i++) {
-            strings = br.readLine().split(" ");
-            for(int j=0; j<m; j++) {
-                map[i][j] = Integer.parseInt(strings[j]);
-                if(map[i][j]==-1) {
-                    acRow = i;
-                }
-            }
-        }
-        for(int i=0; i<t; i++) {
+    public static void solve() {
+        for(int t=0; t<T; t++) {
             spread();
-            move1(acRow - 1);
-            move2(acRow);
+//            printBoard();
+            clean();
+//            break;
+//            printBoard();
         }
 
         int ans = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-//                System.out.print(map[i][j] + " ");
-                if(map[i][j]==-1) continue;
-                ans += map[i][j];
+                if(board[i][j] > 0) {
+                    ans += board[i][j];
+                }
             }
-//            System.out.println();
         }
         System.out.println(ans);
+    }
 
+    public static void clean() {
+        rotateReverse(cleaner.p1);
+        rotate(cleaner.p2);
+    }
+
+    public static void rotateReverse(Point p) {
+        int tmp = board[p.x][m-1];
+        // -->
+        for(int j=m-2; j>=0; j--) {
+            board[p.x][j+1] = board[p.x][j];
+        }
+        // 아래
+        for(int i=p.x-1; i>=0; i--) {
+            board[i+1][0] = board[i][0];
+        }
+        // <--
+        for(int j=1; j<m; j++) {
+            board[0][j-1] = board[0][j];
+        }
+        // 위
+        for(int i=1; i<p.x; i++) {
+            board[i-1][m-1] = board[i][m-1];
+        }
+        board[p.x-1][m-1] = tmp;
+        board[p.x][p.y] = 0;
+    }
+
+    public static void rotate(Point p) {
+        int tmp = board[p.x][m-1];
+        // -->
+        for(int j=m-2; j>=0; j--) {
+            board[p.x][j+1] = board[p.x][j];
+        }
+        // 위
+        for(int i=p.x+1; i<n; i++) {
+            board[i-1][0] = board[i][0];
+        }
+        // <--
+        for(int j=1; j<m; j++) {
+            board[n-1][j-1] = board[n-1][j];
+        }
+        // 아래
+        for(int i=n-2; i>p.x; i--) {
+            board[i+1][m-1] = board[i][m-1];
+        }
+        board[p.x+1][m-1] = tmp;
+        board[p.x][p.y] = 0;
     }
 
     public static void spread() {
-        int[][] temp = new int[n][m];
+        int[][] tmp = new int[n][m];
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(map[i][j]<1) continue;
-                int sum = 0;
-                for(int dir=0; dir<4; dir++) {
-                    int ni = i + dx[dir];
-                    int nj = j + dy[dir];
-                    if(ni<0 || nj<0 || ni>=n || nj>=m) continue;
-                    if(map[ni][nj]==-1) continue;
-                    temp[ni][nj] += map[i][j]/5;
-                    sum += map[i][j]/5;
+                if(board[i][j] > 0) {
+                    int sum = 0;
+                    int num = board[i][j] / 5;
+                    for(int dir=0; dir<4; dir++) {
+                        int nx = i + DX[dir];
+                        int ny = j + DY[dir];
+                        if(!isInner(nx, ny)) continue;
+                        if(isCleaner(nx, ny)) continue;
+                        tmp[nx][ny] += num;
+                        sum += num;
+                    }
+                    tmp[i][j] += (board[i][j] - sum);
                 }
-                temp[i][j] -= sum;
             }
         }
+        board = tmp;
+    }
+
+    public static void printBoard() {
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                map[i][j] += temp[i][j];
+                sb.append(board[i][j]).append(" ");
             }
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 
-    public static void move1(int row1) {
+    public static boolean isInner(int x, int y) {
+        return 0<=x && 0<=y && x<n && y<m;
+    }
 
-        int i = row1-1;
-        while(i>=0) {
-            if(map[i+1][0] == -1) map[i][0] = 0;
-            else {
-                map[i+1][0] = map[i][0];
+    public static boolean isCleaner(int x, int y) {
+        Point p = new Point(x, y);
+        if(cleaner.p1.equals(p) || cleaner.p2.equals(p)) return true;
+        return false;
+    }
+
+    public static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] n_m_t = br.readLine().split(" ");
+            n = Integer.parseInt(n_m_t[0]);
+            m = Integer.parseInt(n_m_t[1]);
+            T = Integer.parseInt(n_m_t[2]);
+            board = new int[n][m];
+            Point p1 = null;
+            Point p2 = null;
+            boolean flag = false;
+            for(int i=0; i<n; i++) {
+                String[] tmp = br.readLine().split(" ");
+                for(int j=0; j<m; j++) {
+                    board[i][j] = Integer.parseInt(tmp[j]);
+                    if(board[i][j] == -1) {
+                        if(!flag) {
+                            p1 = new Point(i, j);
+                            flag = true;
+                        } else {
+                            p2 = new Point(i, j);
+                        }
+                        board[i][j] = 0;
+                    }
+                }
             }
-            i--;
+            cleaner = new Cleaner(p1, p2);
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!!!!");
+            throw new RuntimeException(e);
         }
-        int j = 1;
-        while(j < m) {
-            map[0][j-1] = map[0][j];
-            j++;
-        }
-        i=1;
-        while(i<=row1) {
-            map[i-1][m-1] = map[i][m-1];
-            i++;
-        }
-        j = m-1;
-        while(j>1) {
-            map[row1][j] = map[row1][j-1];
-            j--;
-        }
-        map[row1][j] = 0;
     }
 
-    public static void move2(int row2) {
-
-        int i = row2+1;
-        map[i++][0] = 0;
-        while(i<n) {
-            map[i-1][0] = map[i][0];
-            i++;
-        }
-
-        int j = 1;
-        while(j<m) {
-            map[n-1][j-1] = map[n-1][j];
-            j++;
-        }
-
-        i = n-2;
-        while(i>=row2) {
-            map[i+1][m-1] = map[i][m-1];
-            i--;
-        }
-
-        j=m-2;
-        while(j>0) {
-            map[row2][j+1] = map[row2][j];
-            j--;
-        }
-        map[row2][j+1] = 0;
-    }
 }
