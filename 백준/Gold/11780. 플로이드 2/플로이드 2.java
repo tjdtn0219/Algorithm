edb1cc5aa38970168da525766e8d0c439e000d53
Jang Seongsu <76704436+tjdtn0219@users.noreply.github.com>
2025-11-06 12:35:41 +0900

    [Gold II] Title: 플로이드 2, Time: 536 ms, Memory: 57352 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.java
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.java
@@ -1,39 +1,26 @@
-import java.io.*;
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static final int INF = 100 * 100000 + 5;
+    static final int INF = 100 * 100000 + 5;
 
-    public static void main(String[] args) throws IOException {
+    static int n, m;
+    static int[][] dis;
+    static int[][] nxt;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-
-        int[][] dis = new int[n+1][n+1];
-        for(int i=0; i<=n; i++) {
-            Arrays.fill(dis[i], INF);
-        }
-
-        for(int i=1; i<=n; i++) {
-            dis[i][i] = 0;
-        }
-
-        int[][] nxt = new int[n+1][n+1];
-        for(int i=0; i<m; i++) {
-            String[] strings = br.readLine().split(" ");
-            int a = Integer.parseInt(strings[0]);
-            int b = Integer.parseInt(strings[1]);
-            int c = Integer.parseInt(strings[2]);
-            dis[a][b] = Math.min(dis[a][b], c);
-            nxt[a][b] = b;
-        }
-
+    public static void solve() {
         for(int k=1; k<=n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
+                    if(i == j) continue;
                     if(dis[i][j] > dis[i][k] + dis[k][j]) {
                         dis[i][j] = dis[i][k] + dis[k][j];
                         nxt[i][j] = nxt[i][k];
@@ -52,30 +39,50 @@ public class Main {
 
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
-//                if(dis[i][j]==0 || dis[i][j]==INF) {
-//                    sb.append("0\n");
-//                    continue;
-//                }
-                if(nxt[i][j]==0) {
+                if(nxt[i][j] == 0) {
                     sb.append("0\n");
                     continue;
                 }
-                ArrayList<Integer> list = new ArrayList<>();
+                List<Integer> list = new ArrayList<>();
                 int st = i;
-                while(st!=j) {
+                while(st != j) {
                     list.add(st);
                     st = nxt[st][j];
                 }
                 list.add(j);
-                sb.append(list.size() + " ");
-                for(Integer num : list)
-                    sb.append(num + " ");
+                sb.append(list.size());
+                for(int num : list) {
+                    sb.append(" " + num);
+                }
                 sb.append("\n");
             }
-
         }
 
-        System.out.print(sb.toString());
-
+        System.out.println(sb);
     }
+
+    public static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());
+            m = Integer.parseInt(br.readLine());
+            dis = new int[n+1][n+1];
+            nxt = new int[n+1][n+1];
+            for(int i=0; i<=n; i++) {
+                Arrays.fill(dis[i], INF);
+            }
+            for(int i=0; i<m; i++) {
+                String[] a_b_c = br.readLine().split(" ");
+                int a = Integer.parseInt(a_b_c[0]);
+                int b = Integer.parseInt(a_b_c[1]);
+                int c = Integer.parseInt(a_b_c[2]);
+                dis[a][b] = Math.min(dis[a][b], c);
+                nxt[a][b] = b;
+            }
+        } catch (Exception e) {
+            System.out.println("INPUT ERROR!!!!!!");
+            throw new RuntimeException(e);
+        }
+    }
+
 }
